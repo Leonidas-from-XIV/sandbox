7c850769c2fe52cfcb6ec18ac62a7ac2268cc707
Marek Kubica <marek@xivilization.net>
2005-03-12 18:02:34 +0000

    Documented

--- inetdxmlrpc.py
+++ inetdxmlrpc.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python2.4
 # -*- encoding: latin-1 -*-
+"""A small XML-RPC Server running under control
+of the internet superserver inetd.
+
+Configuring:
+Add this line to your inetd.conf
+embedxmlrpc     stream  tcp     nowait  user   /usr/sbin/tcpd  inetdxmlrpc.py
+Where user is the user to execute the script and 
+inetdxmlprc.py the path to the script.
+
+and this line to your services.conf
+embedxmlrpc     7373/tcp                        # standalone XML-RPC server
+there 7373 will be the port
+
+You have to restart your inetd.
+"""
 import sys, xmlrpclib
 
 def sumAndDifference(a, b):
