276e824ba7394649f6dbc61d0d2b1f081b27b96c
Marek Kubica <marek@xivilization.net>
2005-04-10 12:07:40 +0000

    Reimplemented wrapping, now it is *much* cleaner

--- glwrap.py
+++ glwrap.py
@@ -3,57 +3,43 @@
 """A namespace wrapper for opengl"""
 
 class OGLWrappers(object):
-    """Wrapper
-    dir(wrapped class) --> glwrap.(wrapped class).attrs.keys()"""
-    def __getattr__(self, name):
-        try:
-            return self.attrs[name]
-        except KeyError:
-            raise AttributeError("%s has no attribute '%s'" % (self.wrapped, name))
-        
-    def replace(self, module, funcprefix, constprefix):
-        """Returns a dictionary with renamed variables.
-        You need to specify the module, the prefix of the functions
-        and the prefix of the constants to be removed."""
-        attrs = {}
+    """Wrapper"""
+    def __replace(self, module, funcprefix, constprefix):
+        """Replaces prefixes"""
         for name in module.__dict__.keys():
             if name.startswith(funcprefix):
-               attrs[name[len(funcprefix):]] = module.__dict__[name]
+                setattr(self, name[len(funcprefix):], module.__dict__[name])
             elif name.startswith(constprefix):
-                attrs[name[len(constprefix):]] = module.__dict__[name]
+                setattr(self, name[len(constprefix):], module.__dict__[name])
             else:
-                attrs[name] = module.__dict__[name]
-        return attrs
+                setattr(self, name, module.__dict__[name])
 
 class GLWrap(OGLWrappers):
+    """Wrapper for GL"""
     def __init__(self):
         import OpenGL.GL
-        self.wrapped = OpenGL.GL.__name__
-        self.attrs = self.replace(OpenGL.GL, 'gl', 'GL_')
+        self.attrs = self._OGLWrappers__replace(OpenGL.GL, 'gl', 'GL_')
 
 class GLUWrap(OGLWrappers):
+    """Wrapper for GLU"""
     def __init__(self):
         import OpenGL.GLU
-        self.wrapped = OpenGL.GLU.__name__
-        self.attrs = self.replace(OpenGL.GLU, 'glu', 'GLU_')
+        self.attrs = self._OGLWrappers__replace(OpenGL.GLU, 'glu', 'GLU_')
 
 class GLUTWrap(OGLWrappers):
     def __init__(self):
         import OpenGL.GLUT
-        self.wrapped = OpenGL.GLUT.__name__
-        self.attrs = self.replace(OpenGL.GLUT, 'glut', 'GLUT_')
+        self.attrs = self._OGLWrappers__replace(OpenGL.GLUT, 'glut', 'GLUT_')
 
 class GLEWrap(OGLWrappers):
     def __init__(self):
         import OpenGL.GLE
-        self.wrapped = OpenGL.GLE.__name__
-        self.attrs = self.replace(OpenGL.GLE, 'gle', 'GLE_')
+        self.attrs = self._OGLWrappers__replace(OpenGL.GLE, 'gle', 'GLE_')
 
 class WGLWrap(OGLWrappers):
     def __init__(self):
         import OpenGL.WGL
-        self.wrapped = OpenGL.WGL.__name__
-        self.attrs = self.replace(OpenGL.WGL, 'wgl', 'WGL_')
+        self.attrs = self._OGLWrappers__replace(OpenGL.WGL, 'wgl', 'WGL_')
         
 gl = GLWrap()
 glu = GLUWrap()
