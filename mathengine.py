40319befbe6e4913e98218267c59b5d0cd436e87
Marek Kubica <marek@xivilization.net>
2005-04-18 12:46:11 +0000

    Menubar with separators and handlers

--- mathengine.py
+++ mathengine.py
@@ -18,17 +18,35 @@ class MathWindow(object):
         self.menu = gtk.MenuItem('Mode')
         self.mode_menu = gtk.Menu()
         self.menu.set_submenu(self.mode_menu)
-        self.exit = gtk.MenuItem('Exit')
-        self.mode_menu.add(self.exit)
-        self.menubar.append(self.menu)
-        #menuitem.set_submenu(self.mode_menu)
         
-        #self.window.add(self.menubar)
+        self.menuitem = gtk.SeparatorMenuItem()
+        self.mode_menu.add(self.menuitem)
+        self.menuitem = gtk.MenuItem('About')
+        self.menuitem.connect('activate', self.OnAbout)
+        self.mode_menu.add(self.menuitem)
+        self.menuitem = gtk.MenuItem('About Mode')
+        self.menuitem.connect('activate', self.OnAboutMode)
+        self.mode_menu.add(self.menuitem)
+        self.menuitem = gtk.MenuItem('Exit')
+        self.menuitem.connect('activate', self.OnExit)
+        self.mode_menu.add(self.menuitem)
+        
+        self.menubar.append(self.menu)
+        
         self.window.show_all()
     
     def delete_event(self, widget, event, data=None):
         gtk.main_quit()
         return False
+    
+    def OnExit(self, widget):
+        gtk.main_quit()
+    
+    def OnAbout(self, widget):
+        pass
+    
+    def OnAboutMode(self, widget):
+        pass
 
 def main():
     mw = MathWindow()
