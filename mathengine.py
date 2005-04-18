99043b921d444187d3f189793519057c5953a89c
Marek Kubica <marek@xivilization.net>
2005-04-18 12:30:57 +0000

    Created menubar

--- mathengine.py
+++ mathengine.py
@@ -9,6 +9,21 @@ class MathWindow(object):
         self.window.set_default_size(350, 100)
         self.window.connect('delete_event', self.delete_event)
         self.window.set_title('MathEngine')
+        
+        self.vbox = gtk.VBox()
+        self.window.add(self.vbox)
+        self.menubar = gtk.MenuBar()
+        self.vbox.pack_start(self.menubar, expand=False)
+        
+        self.menu = gtk.MenuItem('Mode')
+        self.mode_menu = gtk.Menu()
+        self.menu.set_submenu(self.mode_menu)
+        self.exit = gtk.MenuItem('Exit')
+        self.mode_menu.add(self.exit)
+        self.menubar.append(self.menu)
+        #menuitem.set_submenu(self.mode_menu)
+        
+        #self.window.add(self.menubar)
         self.window.show_all()
     
     def delete_event(self, widget, event, data=None):
