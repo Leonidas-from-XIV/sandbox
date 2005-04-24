377a469ffcbd6745ca886e063de6810efa160d6f
Marek Kubica <marek@xivilization.net>
2005-04-24 15:43:55 +0000

    Implemented bonus system

--- vbalimit.py
+++ vbalimit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: latin-1 -*-
 
-import optparse, subprocess, os
+import optparse, subprocess, os, pickle
 import gtk, pango, gobject
 
 apicall = True
@@ -26,10 +26,34 @@ def exit(process):
 
 def main():
     left = [0, 0, 5]
+    left = bonus2time()
+    #time2bonus(left)
     cw = CountdownWindow(left)
     gtk.main()
+    
+def bonus2time(perpoint=30, filename='bonusfile.pickle'):
+    f = file(filename, 'r')
+    points = pickle.load(f)
+    f.close()
+    
+    seconds = perpoint * points
+    minutes = seconds / 60
+    seconds = seconds - minutes * 60
+    hours = minutes / 60
+    minutes = minutes - hours * 60
+    return [hours, minutes, seconds]
+
+def time2bonus(t, perpoint=30, filename='bonusfile.pickle'):
+    seconds = t[0] * 60 * 60 + t[1] * 60 + t[2]
+    bonus = seconds / perpoint
+    
+    f = file(filename, 'w')
+    pickle.dump(bonus, f)
+    f.close()
+    
 
 class TimeoutError(Exception):
+    """A self-defined exception, raised when timeouts occur"""
     pass
 
 class CountdownWindow(object):
@@ -57,12 +81,12 @@ class CountdownWindow(object):
     
     def update(self):
         try:
-            print 'decreasing'
+            #print 'decreasing'
             self.decrease_time()
             self.label.set_text(self.list2time(self.timeleft))
             return True
         except TimeoutError:
-            print 'exiting'
+            #print 'exiting'
             exit(self.process)
             self.terminated = True
             return False
@@ -71,22 +95,23 @@ class CountdownWindow(object):
         """Quitting the window"""
         if not self.terminated:
             exit(self.process)
+            time2bonus(self.timeleft)
         gtk.main_quit()
         return False
     
     def list2time(self, lst):
         hours = ''
-        if lst[0] < 9:
+        if lst[0] < 10:
             hours += '0'
         hours += str(lst[0])
         
         minutes = ''
-        if lst[1] < 9:
+        if lst[1] < 10:
             minutes += '0'
         minutes += str(lst[1])
         
         seconds = ''
-        if lst[2] < 9:
+        if lst[2] < 10:
             seconds += '0'
         seconds += str(lst[2])
         
