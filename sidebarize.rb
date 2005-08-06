2e15a2b64aa6f2de98330e288b0633428e4f9455
Marek Kubica <marek@xivilization.net>
2005-08-06 23:50:18 +0000

    Made the program OO

--- sidebarize.rb
+++ sidebarize.rb
@@ -1,34 +1,54 @@
 #!/usr/bin/env ruby
 
-require 'cgi'
+# import support for all kinds of feeds
 require 'rss/0.9'
 require 'rss/1.0'
 require 'rss/2.0'
+# import support for encodings
 require 'iconv'
+# import support for CGI
+require 'cgi'
 
-def from_file(filename)
-    f = File::open(filename)
-    content = f.read
-    f.close
-    return content
-end
-
-def to_html(feeddata)
-    # header
-    puts %q(<html><head><title>Sidebar</title></head><body>)
-    
-    feeddata.each do |item|
-        print '<p><a href="'
-        print item['link']
-        print '">'
-        print convert_entity(item['title'])
-        #print item['title']
-        puts '</a></p>'
-        
+class FeedConverter
+    def initialize
+        @xml_source = ''
+        @feeddata = []
     end
     
-    # footer
-    puts %q(</body></html>)
+    def from_file(filename)
+        f = File::open(filename)
+        @xml_source = f.read
+        f.close
+    end
+    
+    def parse
+        feed = RSS::Parser.parse(@xml_source)
+    
+        feed.items.each do |item|
+            itemdata = {}
+            itemdata['title'] = item.title
+            itemdata['link'] = item.link
+            @feeddata << itemdata
+        end
+    end
+    
+    # Output HTML from the internal data structure
+    def to_html
+        # header
+        puts %q(<html><head><title>Sidebar</title></head><body>)
+    
+        @feeddata.each do |item|
+            print '<p><a href="'
+            print item['link']
+            print '">'
+            print convert_entity(item['title'])
+            #print item['title']
+            puts '</a></p>'
+        end
+    
+        # footer
+        puts %q(</body></html>)
+    end
 end
 
 def convert_entity(text)
@@ -50,17 +70,11 @@ def convert_entity(text)
 end
 
 def main
-    xml_source = from_file('sd.xml')
-    feed = RSS::Parser.parse(xml_source)
+    fc = FeedConverter.new
+    fc.from_file('sd.xml')
+    fc.parse
+    fc.to_html
     
-    feeddata = []
-    feed.items.each do |item|
-        itemdata = {}
-        itemdata['title'] = item.title
-        itemdata['link'] = item.link
-        feeddata << itemdata
-    end
-    to_html(feeddata)
 end
 
 main if __FILE__ == $0
