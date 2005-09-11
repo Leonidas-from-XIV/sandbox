d81888eeaa5e609056212b7f9ee7ad97c40eaabe
Marek Kubica <marek@xivilization.net>
2005-09-11 11:20:47 +0000

    Added handling of keyboard entries, thanks to jkroon on #pygtk for the widget.activate() tip

--- samplesonic.py
+++ samplesonic.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 # -*- encoding: latin-1 -*-
 import pySonic
-import gtk
+import gtk, gobject
 
 class SampleWindow(object):
-    keypad = [ 7, 8, 9,
+    keypad = [7, 8, 9,
                     4, 5, 6,
                     1, 2, 3]
     def __init__(self):
         self.window = gtk.Window()
         self.window.connect('delete_event', gtk.main_quit)
-        self.window.set_title('Achromatic')
-        self.window.set_size_request(300, 300)
+        self.window.connect('key_press_event', self.typed)
+        self.window.set_title('Samplesonic')
+        self.window.set_size_request(200, 200)
         self.layout = gtk.Table()
         
         self.sonic_init()
         
         # create the buttons
+        self.samplerbuttons = {}
         inrow = 0
         row = 0
         for i in self.keypad:
-            cmd = gtk.Button('_'+ str(i))
+            cmd = gtk.Button(str(i))
             cmd.connect('clicked', self.samplebutton)
+            self.samplerbuttons[str(i)] = cmd
         
             self.layout.attach(cmd, inrow, inrow + 1, row, row + 1)
             
@@ -39,11 +42,19 @@ class SampleWindow(object):
     
     def samplebutton(self, widget):
         #print widget
-        sound = widget.get_label()[1:]
+        sound = widget.get_label()
+        print sound
         
         self.src = pySonic.Source()
-        self.src.Sound = pySonic.FileSample('hell.mp3')
+        self.src.Sound = pySonic.FileSample('800.ogg')
+        #self.src.Sound = pySonic.MemorySample("""Zats""" * 800, 1, 8, 800)
         self.src.Play()
+    
+    def typed(self, widget, event):
+        """The user typed a key"""
+        key = event.string
+        button = self.samplerbuttons[key]
+        button.activate()
         
 
 def main():
