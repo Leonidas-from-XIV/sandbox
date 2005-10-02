0309eb67150c1f4cb59a993136179dd26ae7456a
Marek Kubica <marek@xivilization.net>
2005-10-02 08:25:04 +0000

    Added support for Ruby RDoc comments and Python docstrings

--- codelines.rb
+++ codelines.rb
@@ -2,17 +2,56 @@
 
 # Let's check how many lines are really code, not blanks or comments
 def main
-    codelines = 0
-    alllines = 0
+    lines_code = 0
+    lines_all = 0
+    in_docstring = false
+    
     ARGF.each do |line|
-        alllines += 1
+        lines_all += 1
         line.strip!
-        codelines += 1 unless line =~ /^#/ or line.empty?
+	
+        #codelines += 1 unless line =~ /^#/ or line.empty?
+	
+	# ruby rdoc
+	if line =~ /^=begin/
+	    in_docstring = true
+	elsif line =~ /^=end/
+	    in_docstring = false
+	    lines_code -= 1
+	end
+	
+	# python docstrings
+	if line =~ /^""".*"""$/
+	    # a one-line docstring: there are no states
+	    #+so just remove one line from the lines 
+	    #+being marked code
+	    lines_code -= 1
+	elsif line =~ /^"""/
+	    # a multiline docstring (the beginning or the end)
+	    if in_docstring
+	        in_docstring = false
+		lines_code -= 1
+	    else
+		in_docstring = true
+	    end
+	elsif line =~ /"""$/
+	    # end of an multiline string
+	    in_docstring = false
+	    lines_code -= 1
+	end
+	
+	    
+        if line =~ /^#/ or line.empty?
+            # line starts with a # - a comment - ignore it
+	else
+	    #puts '[Matched]' + line unless in_docstring
+	    lines_code += 1 unless in_docstring
+	    #puts line unless in_docstring
+        end
+        
     end
     
-    puts "#{codelines} lines of #{alllines} lines are sourcecode, that's #{((codelines / alllines.to_f) * 100).to_i}%"
+    puts "#{lines_code} lines of #{lines_all} lines are sourcecode, that's #{((lines_code / lines_all.to_f) * 100).to_i}%"
 end
 
-if __FILE__ == $0
-    main()
-end
+main if __FILE__ == $0
