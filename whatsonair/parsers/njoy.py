b3da6141398cd671d7116016f7f75f9098ee9a78
Marek Kubica <marek@xivilization.net>
2005-10-08 19:38:45 +0000

    Added NJoy parser

---
+++ whatsonair/parsers/njoy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: latin-1 -*-
+
+import base
+
+class NJoyParser(base.StationBase):
+    """NJoy"""
+    
+    __station__ = 'NJoy'
+    __version__ = '0.1.1'
+    __versiontuple__ = base.splitver(__version__)
+    
+    def __init__(self, url='http://www1.n-joy.de/pages_special/0,,SPM2156,00.html'):
+        base.StationBase.__init__(self, url)
+    
+    def parse(self):
+        """Call feed first"""
+        artists = self.cut_content('<td headers="headerB">', '\n</td>')
+        titles = self.cut_content('<td headers="headerC">', '</td>')
+        both = zip(artists, titles)
+        
+        self.artist, self.title = both[-1]
+    
+    def current_track(self):
+        return "%s - %s" % (self.capstext(self.artist), self.title)
+
+Parser = NJoyParser
+
+if __name__ == '__main__':
+    base.test_parser(Parser, 'njoy.html')
+    
