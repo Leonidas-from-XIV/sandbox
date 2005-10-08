65654b9982a28ab22a2cbb085ac66477db939e97
Marek Kubica <marek@xivilization.net>
2005-10-08 11:48:06 +0000

    Added parsers for HR3 and YouFM

---
+++ whatsonair/parsers/hr3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: latin-1 -*-
+
+import base
+
+class HR3Parser(base.StationBase):
+    """HR3"""
+    
+    __station__ = 'HR3'
+    __version__ = '0.1.2'
+    __versiontuple__ = base.splitver(__version__)
+    
+    def __init__(self, url='http://www3.admin.hr-online.de/playlist/playlist.php?tpl=hr3neu'):
+        base.StationBase.__init__(self, url)
+    
+    def parse(self):
+        """Call feed first"""
+        both_rex = self.create_regexp('<td bgcolor="#ffffff">', '</td>')
+        both = both_rex.findall(self.pagecontent)
+        # every second hit is an artist
+        artists = both[::2]
+        # te reast are titles
+        tracks = both[1::2]
+        bound = zip(artists, tracks)
+        
+        self.artist, self.title = bound[0]
+    
+    def current_track(self):
+        return "%s - %s" % (self.artist, self.title)
+
+Parser = HR3Parser
+
+if __name__ == '__main__':
+    base.test_parser(Parser, 'hr3.html')
+    
---
+++ whatsonair/parsers/youfm.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding: latin-1 -*-
+"""This plugin depends on the HR3 plugin, as it uses its parser internally"""
+
+import base, hr3
+
+class YouFmParser(hr3.HR3Parser):
+    """YouFM"""
+    
+    __station__ = 'YouFM'
+    __version__ = '0.1.2'
+    __versiontuple__ = base.splitver(__version__)
+    
+    def __init__(self, url='http://www3.admin.hr-online.de/playlist/playlist.php?tpl=youfm'):
+        hr3.HR3Parser.__init__(self, url)
+
+Parser = YouFmParser
+
+if __name__ == '__main__':
+    base.test_parser(Parser, 'youfm.html')
+    
