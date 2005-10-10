a6464729ae93bb12c8283745c1992aeb6da77662
Marek Kubica <marek@xivilization.net>
2005-10-10 16:23:34 +0000

    Improved the main parser

--- whatsonair/parsers/virgin.py
+++ whatsonair/parsers/virgin.py
@@ -7,20 +7,26 @@ class VirginParser(base.StationBase):
     """Parser for Virgin Radio: the music we all love"""
     
     __station__ = 'Virgin'
-    __version__ = '0.1.0'
+    __version__ = '0.1.1'
     
     def __init__(self, url='http://mangle.smgradio.com/vr.js'):
         base.StationBase.__init__(self, url)
     
     def parse(self):
-        """Call feed first"""
+        """Parses the files served by Mrs Mangle"""
         info_string = self.cut_content('var gimpdata="', '"')[0]
         informations = info_string.split('~')
         self.artist = informations[0]
         self.title = informations[2]
     
     def current_track(self):
-        return "%s - %s" % (self.capstext(self.artist), self.capstext(self.title))
+        """Returns the current playing artist
+        in the format: artist - title.
+        Returns None when there is nothing interessting to display"""
+        if self.title != '' or self.title != '':
+            return "%s - %s" % (self.artist, self.title)
+        else:
+            return None
 
 Parser = VirginParser
 
