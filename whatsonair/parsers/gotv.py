210b6ef1fd6981201d1b53f069b1a875222a3113
Marek Kubica <marek@xivilization.net>
2005-11-16 17:37:19 +0000

    Added parser for gotv

---
+++ whatsonair/parsers/gotv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- encoding: latin-1 -*-
+
+import base, re
+
+class GoTvParser(base.StationBase):
+    """GoTv"""
+    
+    __station__ = 'GoTv'
+    __version__ = '0.1.0'
+    
+    def __init__(self, url='http://www.gotv.at/titel_main.php'):
+        base.StationBase.__init__(self, url)
+    
+    def parse(self):
+        """Call feed first"""
+        
+        track = self.cut_content("\('VideoJetzt'\).innerHTML != '", "'\)")[0]
+        
+        # remove eventual html tags
+        rex = re.compile("<.*?>")
+        track = rex.sub('', track)
+        
+        # set informations
+        self.artist, self.title = track.split(' - ')
+    
+    def current_track(self):
+        return "%s - %s" % (self.artist, self.title)
+
+Parser = GoTvParser
+
+if __name__ == '__main__':
+    base.test_parser(Parser, 'gotv.html')
+    
