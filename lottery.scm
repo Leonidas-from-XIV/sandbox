5b0dbe0ad13edeb28039ea2adea116d85e348a8b
Marek Kubica <marek@xivilization.net>
2008-05-04 15:35:57 +0200

    Using default parameters now via `kw.ss`, got rid of unneccessary find-item

--- lottery.scm
+++ lottery.scm
@@ -1,46 +1,35 @@
-; Program that gets random numbers for lottery
+; Gets n random numbers for lottery
 ; 2008 by Marek Kubica
-; Kids, don't try this at home, there is enough space for improvement.
+; Don't be surprised if there is strange stuff, this is just for
+; autoeducational purposes
+
+(require (lib "kw.ss"))
 
 (define list-choice
   (lambda (list)
     "Chooses a random element from a list"
     (list-ref list (random (length list)))))
 
-(define range-rec
-  (lambda (start stop lat)
-    (if (= start stop) '()
-        (cons start (range-rec (+ start 1) stop lat)))))
-
-;(range-rec 0 10 '())
-
 (define range
-  (lambda (start stop)
+  (lambda/kw (#:optional start stop [lat '()])
     "Returns a list starting with start and adding up to stop"
-    (range-rec start stop '())))
+    (if (= start stop) '()
+        (cons start (range (+ start 1) stop lat)))))
 
-;(range 1 (+ 49 1))
-
-(define find-item-rec
-  (lambda (lat item index)
-    (if (equal? item (car lat)) index
-        (find-item-rec (cdr lat) item (+ index 1)))))
-
-(define find-item
-  (lambda (lat item)
-    "Finds the index of a specific item in the list"
-    (find-item-rec lat item 0)))
+;(range 0 10)
 
 ; funnily enough, The Litte Schemer defines a nearly identical function like
 ; this one - called `rember` (p. 41)
 (define remove-from-list
   (lambda (lat atom)
+    "Removes an item from a list"
     (if (null? lat) '()
-        (if (= (car lat) atom) (remove-from-list (cdr lat) atom)
+        (if (equal? (car lat) atom) (remove-from-list (cdr lat) atom)
             (cons (car lat) (remove-from-list (cdr lat) atom))))))
 
 (define lottery-numbers
   (lambda (choices to-find)
+    "Chooes to-find unique numbers from choices"
     (if (= to-find 0) '()
         (let ((chosen (list-choice choices)))
           (cons chosen
