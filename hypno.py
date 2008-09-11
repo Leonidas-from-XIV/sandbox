8e6a5724111ca6a1e3148a595e7fd0849c0e260c
Marek Kubica <marek@xivilization.net>
2008-09-11 16:14:19 +0200

    Made rotation actually work

--- hypno.py
+++ hypno.py
@@ -9,6 +9,7 @@ class HypnoticWindow(object):
         self.window = gtk.Window()
         self.window.connect('delete_event', gtk.main_quit)
         self.width, self.height = width, height
+        self.orientation = 0
 
         self.canvas = gnomecanvas.Canvas()
         self.canvas.set_size_request(width, height)
@@ -23,17 +24,18 @@ class HypnoticWindow(object):
 
         self.window.show_all()
 
-    def create_spiral(self, x, y):
+    def create_spiral(self, x, y, rotate=0):
+        """rotate takes an angle in radian."""
         self.path = [
             (self.width / 2, self.height / 2)
         ]
-        for dummy in xrange(500):
-            r = math.hypot(x, y) + 0.4
-            a = math.atan2(y, x) + 0.2
-            x = r * math.cos(a)
-            y = r * math.sin(a)
-            self.path.append(
-                (x + self.width / 2, y + self.height / 2))
+        a = 0.03
+        for t in frange(0, 10 * math.pi, 0.1):
+            r = a * t
+            angle = t + rotate
+            x = r * math.cos(angle) * 200
+            y = r * math.sin(angle) * 200
+            self.path.append((x + 200, y + 200))
 
         self.render_path(self.path)
 
@@ -44,24 +46,19 @@ class HypnoticWindow(object):
         self.bpath.set_bpath(path_def)
 
     def rotate(self, x0, y0, d):
-        coords = list()
-        old = self.path
-        for i in range(0, len(old), 2):
-            try:
-                x, y = old[i][0] - x0, old[i + 1][1] - y0
-            except IndexError:
-                continue
-            r = math.hypot(y, x)
-            a = math.atan2(y, x) + d
-            coords.append((x0 + r * math.cos(a), y0 + r * math.sin(a)))
-        self.render_path(coords)
-        self.path = coords
+        self.orientation += d
+        self.create_spiral(0, 0, self.orientation)
+        return True
+
+def frange(start, stop, step):
+    while start < stop:
+        yield start
+        start += step
 
 def main():
     win = HypnoticWindow(400, 400)
     win.create_spiral(0, 0)
-    #win.rotate(0, 0, 0.1)
-    #gobject.timeout_add(100, win.rotate, 0, 0, 0.1)
+    gobject.timeout_add(10, win.rotate, 0, 0, 0.1)
     gtk.main()
 
 if __name__ == '__main__':
