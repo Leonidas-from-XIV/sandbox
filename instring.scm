ec1ba2671ca37d6b94f5b31cdb9d664d64a5828a
Marek Kubica <marek@xivilization.net>
2008-10-05 12:59:57 +0200

    More comments in the code

--- instring.scm
+++ instring.scm
@@ -1,5 +1,9 @@
 #lang scheme
+; instead of every and any PLT also supports andmap and ormap respectively
+; thus making SRFI 1 unneccessary
 (require srfi/1)
+; alternatively there is SFRI 13 string-every which saves a let-binding,
+; but does not make things considerably easier
 
 (define chars-in-string?
   (lambda (chars string)
