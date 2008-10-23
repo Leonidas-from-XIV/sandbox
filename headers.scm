0a39beabf8e124f6541f8378416171afbae5c00e
Marek Kubica <marek@xivilization.net>
2008-10-23 16:52:27 +0200

    Cleanup and nicer display (markup)

--- headers.scm
+++ headers.scm
@@ -1,17 +1,11 @@
 #lang web-server/insta
-(require web-server/private/request-structs)
 
 (define (display-header header)
-  `(li ,(string-append (bytes->string/utf-8 (header-field header))
-                       ": "
-                       (bytes->string/utf-8 (header-value header)))))
-
-;(define (display-header header)
-;  "Header")
+  `(li (strong ,(bytes->string/utf-8 (header-field header) )) 
+       ": "
+       ,(bytes->string/utf-8 (header-value header))))
 
 (define (start request)
-  (display (map display-header (request-headers/raw request)))
   `(html
     (head (title "Request Headers"))
     (body (ul ,@(map display-header (request-headers/raw request))))))
-    ;(body (ul (li "abc") (li "def")))))
