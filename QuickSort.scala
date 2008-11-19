bb2b526e6e215e915113c67319904efe829008f4
Marek Kubica <marek@xivilization.net>
2008-11-20 00:09:12 +0100

    Got rid of useless semicolons

--- QuickSort.scala
+++ QuickSort.scala
@@ -1,21 +1,20 @@
 /* Natural number type */
 class Nat(val value: Int) extends Ordered[Nat] {
-  require(value >= 0);
+  require(value >= 0)
   
   // define comparison operators in one step
-  override def compare(that: Nat): Int = this.value - that.value;
+  override def compare(that: Nat): Int = this.value - that.value
 
-  def toInt() = this.value;
-  override def toString() = this.value.toString();
+  def toInt() = this.value
+  override def toString() = this.value.toString()
 }
 
 object QuickSort {
 
   def main(args: Array[String]) {
-    val unsorted_ints = List(3, 5, 7, 2, 9, 8, 1, 6, 4);
-    val unsorted = (for(item <- unsorted_ints) yield new Nat(item));
-    val sorted = quicksort(unsorted);
-    println(sorted);
+    val unsorted_ints = List(3, 5, 7, 2, 9, 8, 1, 6, 4)
+    val unsorted = (for(item <- unsorted_ints) yield new Nat(item))
+    println(quicksort(unsorted))
   }
 
   def kls(s: List[Nat], p: Nat): List[Nat] =
@@ -28,7 +27,7 @@ object QuickSort {
 
   def quicksort(s: List[Nat]): List[Nat] =
     if (s.length <= 1) s else {
-      val p = s.first;
-      quicksort(kls(s.tail, p)) ++ List(p) ++ quicksort(gls(s.tail, p));
+      val p = s.first
+      quicksort(kls(s.tail, p)) ++ List(p) ++ quicksort(gls(s.tail, p))
     }
 }
