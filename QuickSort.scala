b666e40304fa00bbcca5f9329f4ae792b7e19bdb
Marek Kubica <marek@xivilization.net>
2008-11-19 23:24:24 +0100

    Added Quicksort written in Scala

---
+++ QuickSort.scala
@@ -0,0 +1,33 @@
+/* Natural number type */
+class Nat(val value: Int) extends Ordered[Nat] {
+  require(value >= 0);
+  
+  // define comparison operators in one step
+  override def compare(that: Nat): Int = this.value - that.value;
+
+  def toInt() = this.value;
+}
+
+object QuickSort {
+
+  def main(args: Array[String]) {
+    val unsorted_ints = List(3, 5, 7, 2, 9, 8, 1, 6, 4);
+    val unsorted = (for(item <- unsorted_ints) yield new Nat(item));
+    val sorted = quicksort(unsorted);
+    println((for(item <- sorted) yield item.toInt()));
+  }
+
+  def kls(s: List[Nat], p: Nat): List[Nat] =
+    if (s.length == 0) s else
+      if (s.first <= p) s.first :: kls(s.tail, p) else kls(s.tail, p)
+	
+  def gls(s: List[Nat], p: Nat): List[Nat] =
+    if (s.length == 0) s else
+      if (s.first >= p) s.first :: gls(s.tail, p) else gls(s.tail, p)
+
+  def quicksort(s: List[Nat]): List[Nat] =
+    if (s.length <= 1) s else {
+      val p = s.first;
+      quicksort(kls(s.tail, p)) ++ List(p) ++ quicksort(gls(s.tail, p));
+    }
+}
