d4154f0d874e7babdbd649e8658f923446672a19
Marek Kubica <marek@xivilization.net>
2008-11-28 18:14:01 +0100

    Added the data that is used for the flight system

---
+++ flightplan.scm
@@ -0,0 +1,13 @@
+#lang scheme
+(define default-flights
+  '(("Berlin" "Rom")
+    ("Berlin" "London")
+    ("Berlin" "Paris")
+    ("Rom" "London")
+    ("London" "Madrid")
+    ("London" "Athen")
+    ("Paris" "London")
+    ("Paris" "Madrid")
+    ("Athen" "Berlin")
+    ("Madrid" "Rom")))
+
