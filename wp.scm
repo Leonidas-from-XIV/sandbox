4b5a47f80103c438a6d02ab9489d3207ef309234
Marek Kubica <marek@xivilization.net>
2008-12-18 09:21:31 +0100

    Ported word counting to streams
    It now uses 1/10th of the memory but is slower, roughly 13% in a simple test

--- wp.scm
+++ wp.scm
@@ -19,12 +19,12 @@
 
 ; returns a hashmap mapping words to their frequency
 (define frequencies
-  (foldl (lambda (word hash)
+  (stream-fold (lambda (hash word)
          (let [(current-value (hash-ref hash word 0))]
            (hash-set hash word (add1 current-value))))
        (make-immutable-hash '())
        ; feed in the separated words
-       (string-tokenize (port->string (current-input-port)))))
+       (port->stream-of-tokens)))
 
 (define freq-list (hash-map frequencies
                             (lambda (key value)
