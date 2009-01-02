28eb780c46f88cedecb886a8e659f97a3ee5bb10
Marek Kubica <marek@xivilization.net>
2009-01-02 02:20:06 +0100

    Postfix Scheme macro is now working properly

--- postscheme.scm
+++ postscheme.scm
@@ -1,26 +1,19 @@
-; A try on macros by defining a useless DSL
-; 2008 by Marek Kubica
-; <http://ircbrowse.com/channel/scheme/20080726>
+#lang scheme
+;;;; A try on macros by defining a useless DSL that changes Scheme code from
+;;;; a prefix language to a postfix language. This is all half-jokingly
+;;;; but useful for learning how to use macros.
+;;;;
+;;;; 2009 by Marek Kubica
 
-; transform list (a b c d) to (b c d a)
-(define push-contents
-  (lambda (lst)
-    (append (cdr lst) (list (car lst)))))
+;;; the postfixing macro, that apply the transformation on itself and
+;;; every operand.
+(define-syntax postfixed
+  (syntax-rules ()
+    ((_ (operands ... operator)) (operator (postfixed operands) ...))
+    ((_ atom) atom)))
 
-; transform list (b c d a) to (a b c d)
-(define pop-contents
-  (lambda (lst)
-    (let ((reversed (reverse lst)))
-      (cons (car reversed) (reverse (cdr reversed))))))
-
-; sjamaan & elfs solution to the problem
-; Yeah, it's nice and makes my functions useless
-; at least I can try to get the macro to apply recursively
-(define-syntax postfixed 
-  (syntax-rules () 
-    ((_ (?operands ... ?operator)) (?operator ?operands ...))))
-
-; works as expected
+;; all of these return 5
+(postfixed 5)
 (postfixed (2 3 +))
-; works, but should be (1 2 +) -> apply macro recursively
-(postfixed (2 (+ 1 2) +))
+(postfixed (2 (1 2 +) +))
+(postfixed ((1 1 +) (1 2 +) +))
