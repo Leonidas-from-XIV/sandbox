59025a6ac7f4019a9d4fba7878476c7d88b01285
Marek Kubica <marek@xivilization.net>
2009-03-13 20:40:10 +0100

    Improved every-nth-element, added some code to work with the text

--- gardenfence.scm
+++ gardenfence.scm
@@ -6,17 +6,18 @@
 (require srfi/1)
 
 ;;; returns every nth item of a list, starting with the first
-;;; but don't pass values of n larger than
 (define every-nth-item
   (lambda (lat n)
-    (if (empty? lat) '()
-        (cons (car lat) 
-              (every-nth-item (drop lat n) n)))))
+    (cond [(empty? lat) '()]
+          [(> n (length lat)) (list (car lat))]
+          [else (cons (car lat)
+                      (every-nth-item (drop lat n) n))])))
 
 (define encode
-  (lambda (text depth)
-    (let [(chars (string->list text))]
-      (every-nth-item chars 2))))
+  (lambda (chars times move)
+    (cond [(= times 1) (every-nth-item chars move)]
+          [else (cons (every-nth-item chars move)
+                      (encode (cdr chars) (- times 1) move))])))
 
-(encode "diesisteinklartext" 6)
-(every-nth-item '(1 2 3 4) 2)
+(encode (string->list "diesisteinklartext") 12 10)
+(every-nth-item '(1 2 3 4) 5)
