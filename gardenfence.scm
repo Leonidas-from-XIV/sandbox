645b855a7a70d62c558a5061cc1e4b14fb50e7f0
Marek Kubica <marek@xivilization.net>
2009-03-14 09:59:18 +0100

    Implemented list merging

--- gardenfence.scm
+++ gardenfence.scm
@@ -13,12 +13,35 @@
           [else (cons (car lat)
                       (every-nth-item (drop lat n) n))])))
 
+;;; bad name, this does not actually encode
 (define encode
   (lambda (chars times move)
     (cond [(= times 1) '()]
           [else (cons (every-nth-item chars move)
                       (encode (cdr chars) (- times 1) move))])))
 
+(define merge-phases
+  (lambda (lat)
+    (let* ([len (length lat)]
+           [head (car lat)]
+           ; last is wrong
+           [tail (last lat)])
+      `(,head ,@(merge-all-double lat 1 (- len 1)) ,tail))))
+
+(define merge-double-phases
+  (lambda (lat first second)
+    (let ([head (list-ref lat first)]
+          [tail (list-ref lat second)])
+      (list head tail))))
+
+(define merge-all-double
+  (lambda (lat begin end)
+    (if (>= begin end) '()
+        (cons (merge-double-phases lat begin end)
+              (merge-all-double lat (+ begin 1) (- end 1))))))
+
 ;; 11 = (height * 2) - 1
-(encode (string->list "diesisteinklartext") 11 10)
-(every-nth-item '(1 2 3 4) 5)
+(define a (encode (string->list "diesisteinklartext") 11 10))
+a
+;(every-nth-item '(1 2 3 4) 5)
+(merge-phases a)
