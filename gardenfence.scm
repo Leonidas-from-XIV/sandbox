87c488dc788c3788c0f05a0678733a81aa259965
Marek Kubica <marek@xivilization.net>
2009-03-15 13:41:26 +0100

    Added more documentation for the algorithms

--- gardenfence.scm
+++ gardenfence.scm
@@ -10,12 +10,12 @@
 ;;;;
 ;;;; An example:
 ;;;;
-;;;; 1. d         k         = dk
-;;;; 2.  i       n l        = inl
-;;;; 3.   e     i   a       = eia
-;;;; 4.    s   e     r   t  = sert
-;;;; 5.     i t       t x   = ittx
-;;;; 6.      s         e    = se
+;;;; 1. d         k         = (d k)     = "dk"
+;;;; 2.  i       n l        = (i n l)   = "inl"
+;;;; 3.   e     i   a       = (e i a)   = "eia"
+;;;; 4.    s   e     r   t  = (s e r t) = "sert"
+;;;; 5.     i t       t x   = (i t t x) = "ittx"
+;;;; 6.      s         e    = (s e)     = "se"
 ;;;;
 ;;;; Then the resulting encrypted text is "dkinleiasertittxse",
 ;;;; which results in appending the characters from all lines, starting
@@ -27,7 +27,7 @@
 ;;; for DROP
 (require srfi/1)
 
-;;; returns every nth item of a list, starting with the first
+;;; Returns every nth item of a list, starting with the first
 (define every-nth-item
   (lambda (lat n)
     (cond [(empty? lat) '()]
@@ -35,14 +35,29 @@
           [else (cons (car lat)
                       (every-nth-item (drop lat n) n))])))
 
-;;; creates a list of numbers of all phases
+;;; Creates a list of characters of all phases.
+;;; A phase is basically one half of the zigzag, representing either a
+;;; rising line or a declining line
+;;; 
+;;; Given the example from the header, the result of only declining
+;;; lines would be:
+;;;
+;;; 1. d         k         = (d k)
+;;; 2.  i         l        = (i l)
+;;; 3.   e         a       = (e a)
+;;; 4.    s         r      = (s r)
+;;; 5.     i         t     = (i t)
+;;; 6.      s         e    = (s e)
+;;;
+;;; This function creates both rising and declining lines, but they aren't
+;;; merged, so that they don't form a zigzag yet.
 (define build-phases
   (lambda (chars move times)
     (if (= times 1) (list (every-nth-item chars move))
         (cons (every-nth-item chars move)
               (build-phases (cdr chars) move (- times 1))))))
 
-;;; encrypts a string using the given height to a new, encrypted string
+;;; Encrypts a string using the given height to a new, encrypted string
 (define encrypt
   (lambda (text height)
     (let* ([chars (string->list text)]
@@ -53,7 +68,28 @@
            [unified-phases (merge-phases up/down-phases height)])
       (apply string-append (map list->string unified-phases)))))
 
-;;; merges up and down phases and leaves the top and bottom phases alone
+;;; Merges rising and declinging phases while leaving the top and bottom
+;;; phases alone, as they don't need to be merged:
+;;; 
+;;; It merges the declining lines:
+;;;
+;;; 1. d         k         = (d k)
+;;; 2.  i         l        = (i l)
+;;; 3.   e         a       = (e a)
+;;; 4.    s         r      = (s r)
+;;; 5.     i         t     = (i t)
+;;; 6.      s         e    = (s e)
+;;;
+;;; with the rising lines:
+;;;
+;;; 1.                     = 
+;;; 2.          n          = (n)
+;;; 3.         i           = (i)
+;;; 4.        e         t  = (e t)
+;;; 5.       t         x   = (t x)
+;;; 6.                     = 
+;;;
+;;; so that the zigzag structure from the header is formed
 (define merge-phases
   (lambda (lat height)
     (let* ([len (length lat)]
@@ -66,9 +102,10 @@
       ;; connect top, the merged phases and bottom phases together
       `(,top ,@merged ,bottom))))
 
-;;; helper for merge-phases, does the actual merging
+;;; Helper for merge-phases, does the actual merging
 (define merge-up/down-phases
   (lambda (lat begin end)
+    ;; merges the up and down phase
     (define merge-two-phases
       (lambda (lat first second)
         (let ([head (list-ref lat first)]
@@ -90,9 +127,9 @@
            `(,(car head-lat) ,(car tail-lat) 
                              ,@(interweave (cdr head-lat) (cdr tail-lat)))])))
 
-;;; generates the "gartenzaun" for the given height and text but instead
+;;; Generates the "garden fence" for the given height and text but instead
 ;;; of letters, it uses numbers, so the result can be used to map
-;;; letters back to their original position in the cleartext
+;;; letters back to their original position in the cleartext.
 (define generate-linecodes
   (lambda (text height)
     (let* ([len (string-length text)]
@@ -102,17 +139,30 @@
        (build-phases (build-list len values) move times)
        height))))
 
-;;; flattens the results into a single list, so it can be converted
+;;; Flattens the list of lists into a single list, so it can be converted
 ;;; into a string
 (define flatten-linecode
   (lambda (code)
     (if (null? code) '()
         (append (car code) (flatten-linecode (cdr code))))))
 
-;;; decrypts an encrypted text using the given height as key
+;;; Decrypts an encrypted text using the given height as key
 ;;; works by constructing a zigzag structure but not containing the
 ;;; letters but rather the positions in the original string
 ;;; the positions get mapped to letters to reconstruct the plaintext
+;;;
+;;; Using the example from the header, the fence with numbers looks
+;;; like this
+;;;
+;;; 1. 0         10                = (0 10)
+;;; 2.  1       9  11              = (1 9 11)
+;;; 3.   2     8     12            = (2 8 12)
+;;; 4.    3   7        13      17  = (3 7 13 17)
+;;; 5.     4 6           14  16    = (4 6 14 16)
+;;; 6.      5              15      = (5 15)
+;;;
+;;; The resulting lists get flattened and are used to map the
+;;; characters back to their original, plaintext position.
 (define decrypt
   (lambda (text height)
     (let ([cleartext
