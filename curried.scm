de2766afb4b649705310a9c46f4e14a4e7e8cdd6
Marek Kubica <marek@xivilization.net>
2009-03-25 15:09:17 +0100

    Changed to use the HtDP testing facilities

--- curried.scm
+++ curried.scm
@@ -1,24 +1,30 @@
 #lang scheme
+(require htdp/testing)
 
+;;; a manually curried division function
 (define manually-curried
   (lambda (a)
     (lambda (b)
       (/ a b))))
 
-((manually-curried 1) 2)
-
+;;; three layer deep currying
 (define three-layers
   (lambda (first)
     (lambda (second)
       (lambda (third)
         (+ first second third)))))
 
-(((three-layers 1) 2) 3)
-
+;;; call/curry macro that calls functions recursively
 (define-syntax call/curry
   (syntax-rules ()
     [(_ fun single-arg) (fun single-arg)]
     [(_ fun first-arg ... last-arg) ((call/curry fun first-arg ...) last-arg)]))
 
-(call/curry manually-curried 1 2)
-(call/curry three-layers 1 2 3)
+;;; test results
+(check-expect ((manually-curried 1) 2) (/ 1 2))
+(check-expect (call/curry manually-curried 1 2) (/ 1 2))
+(check-expect (((three-layers 1) 2) 3) 6)
+(check-expect (call/curry three-layers 1 2 3) 6)
+
+;;; generate a report
+(generate-report)
