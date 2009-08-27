1e55f4d241ac7c3268b2365121d57be670dbb6f2
Marek Kubica <marek@xivilization.net>
2009-08-27 23:32:47 +0200

    Added some code from the first little schemer chapters

---
+++ little.scm
@@ -0,0 +1,27 @@
+#lang scheme
+;;;; Implementations of code from The Little Schemer, forth edition
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (list? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      [(null? l) #t]
+      [(atom? (car l)) (lat? (cdr l))]
+      [else #f])))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      [(null? lat) #f]
+      [(eq? (car lat) a) #t]
+      [else (member? a (cdr lat))])))
+
+(define rember
+  (lambda (a lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) a) (cdr lat)]
+      [else (cons (car lat) (rember a (cdr lat)))])))
