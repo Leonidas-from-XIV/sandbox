6727f145bf4d765e75e66d25ccade2d108d84f05
Marek Kubica <marek@xivilization.net>
2009-09-01 17:08:31 +0200

    Added insertR implementation form the book

--- little.scm
+++ little.scm
@@ -37,3 +37,15 @@
       [(null? l) '()]
       [else (cons (caar l)
                   (firsts (cdr l)))])))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [else (cond
+              [(eq? (car lat) old)
+               (cons old
+                     (cons new (cdr lat)))]
+              [else (cons (car lat)
+                          (insertR new old
+                                   (cdr lat)))])])))
