16087f195bfacc36b6848308a3eb9b9c3c6bb019
Marek Kubica <marek@xivilization.net>
2009-09-01 17:23:30 +0200

    Implemented subst2 as described in the book

--- little.scm
+++ little.scm
@@ -66,3 +66,12 @@
       [(eq? (car lat) old) (cons new (cdr lat))]
       [else (cons (car lat)
                   (subst new old (cdr lat)))])))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      [(null? lat) '()]
+      [(or (eq? (car lat) o1) (eq? (car lat) o2))
+       (cons new (cdr lat))]
+      [else (cons (car lat)
+                  (subst2 new o1 o2 (cdr lat)))])))
