64c13aaa16a4bdb4af07a228d7b9251763dfe6be
Marek Kubica <marek@xivilization.net>
2009-09-01 20:20:36 +0200

    Added my implementation of multiinsertR

--- little.scm
+++ little.scm
@@ -83,3 +83,14 @@
       [(eq? (car lat) a) (multirember a (cdr lat))]
       [else (cons (car lat)
                   (multirember a (cdr lat)))])))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old)
+       (cons old
+             (cons new (multiinsertR new old (cdr lat))))]
+      [else (cons (car lat)
+                  (multiinsertR new old
+                           (cdr lat)))])))
