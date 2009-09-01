3e65565b5bf48a55e4686d2d295fb8e647613a94
Marek Kubica <marek@xivilization.net>
2009-09-01 20:24:52 +0200

    Added symetrical operation to multiinsertR: multiinsertL

--- little.scm
+++ little.scm
@@ -94,3 +94,15 @@
       [else (cons (car lat)
                   (multiinsertR new old
                            (cdr lat)))])))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old)
+       (cons new
+             (cons old
+                   (multiinsertL new old (cdr lat))))]
+      [else (cons (car lat)
+                  (multiinsertL new old
+                           (cdr lat)))])))
