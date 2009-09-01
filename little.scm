e1b821f58acebe8f1b25df553a45d187714b6a8e
Marek Kubica <marek@xivilization.net>
2009-09-01 17:12:10 +0200

    Added insertL, nearly symmetrical to insertR

--- little.scm
+++ little.scm
@@ -49,3 +49,14 @@
               [else (cons (car lat)
                           (insertR new old
                                    (cdr lat)))])])))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [else (cond
+              [(eq? (car lat) old)
+               (cons new lat)]
+              [else (cons (car lat)
+                          (insertL new old
+                                   (cdr lat)))])])))
