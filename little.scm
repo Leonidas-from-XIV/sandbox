34676ab36da3967c5a06fd565ba2a78809674f9e
Marek Kubica <marek@xivilization.net>
2009-09-01 17:15:21 +0200

    Simplified the insertR/L code by using more than two cond-branches

--- little.scm
+++ little.scm
@@ -42,21 +42,19 @@
   (lambda (new old lat)
     (cond
       [(null? lat) '()]
-      [else (cond
-              [(eq? (car lat) old)
-               (cons old
-                     (cons new (cdr lat)))]
-              [else (cons (car lat)
-                          (insertR new old
-                                   (cdr lat)))])])))
+      [(eq? (car lat) old)
+       (cons old
+             (cons new (cdr lat)))]
+      [else (cons (car lat)
+                  (insertR new old
+                           (cdr lat)))])))
 
 (define insertL
   (lambda (new old lat)
     (cond
       [(null? lat) '()]
-      [else (cond
-              [(eq? (car lat) old)
-               (cons new lat)]
-              [else (cons (car lat)
-                          (insertL new old
-                                   (cdr lat)))])])))
+      [(eq? (car lat) old)
+       (cons new lat)]
+      [else (cons (car lat)
+                  (insertL new old
+                           (cdr lat)))])))
