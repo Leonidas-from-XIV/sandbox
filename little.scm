e6922c4741427b0cf0da6316b58f536d22d3eca9
Marek Kubica <marek@xivilization.net>
2009-09-01 20:53:13 +0200

    Added multisubst implementation

--- little.scm
+++ little.scm
@@ -106,3 +106,10 @@
       [else (cons (car lat)
                   (multiinsertL new old
                            (cdr lat)))])))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      [(null? lat) '()]
+      [(eq? (car lat) old) (cons new (multisubst new old (cdr lat)))]
+      [else (cons (car lat) (multisubst new old (cdr lat)))])))
