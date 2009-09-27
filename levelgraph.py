924685f610b72b9d0510155e1ecc3db1d4d8d84a
Marek Kubica <marek@xivilization.net>
2009-09-27 17:59:03 +0200

    Added a thread to handle reading from the source

--- levelgraph.py
+++ levelgraph.py
@@ -6,51 +6,41 @@ import matplotlib
 import serial
 import threading
 
-#ser = serial.Serial('/dev/ttyUSB0', 9600)
-
 matplotlib.use('GTKAgg')
 import gobject
 import matplotlib.pyplot as plt
 
 figure = plt.figure()
 axes = figure.add_subplot(111)
-
-value = ""
-throwaway = 10
-
-class Device(threading.Thread):
-    pass
-
-def animate():
-    global value, throwaway
-    x = np.arange(0, 1024, 1)
-    #line, = ax.plot(x, np.sin(x))
-    while True:
-        c = ser.read(1)
-        if c == '\n':
-            try:
-                if throwaway == 20:
-                    value_int = int(value)
-                    print 'integer_value:', value_int
-                    line.set_ydata(value_int)
-                    fig.canvas.draw()
-                    throwaway = 0
-                else:
-                    throwaway += 1
-                value = ""
-            except ValueError:
-                pass
-        elif c == '\r':
-            pass
-        else:
-            value += c
-
 x_points = range(1000)
 y_points = [0 for i in range(1000)]
 line = axes.plot(x_points, y_points, 'b')[0]
 
-def update_graph():
-    value = random.randrange(0, 1024, 1)
+class Device(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.value = 0
+        self.accumulator = []
+        #self.source = serial.Serial('/dev/ttyUSB0', 9600)
+        self.source = open('foo', 'r')
+
+    def run(self):
+        while True:
+            c = self.source.read(1)
+            if c == '\n':
+                try:
+                    self.value = int(''.join(self.accumulator))
+                    self.accumulator = []
+                except ValueError:
+                    pass
+            elif c == '\r':
+                pass
+            else:
+                self.accumulator.append(c)
+
+def update_graph(source):
+    #value = random.randrange(0, 1024, 1)
+    value = source.value
     for i in xrange(10):
         y_points.pop(0)
         y_points.append(value)
@@ -60,8 +50,11 @@ def update_graph():
     figure.canvas.draw()
     return True
 
-#gobject.timeout_add(1, animate)
-#gobject.timeout_add(50, enter)
-gobject.idle_add(update_graph)
-print 'showing'
-plt.show()
+def main():
+    source = Device()
+    source.start()
+    gobject.idle_add(update_graph, source)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
