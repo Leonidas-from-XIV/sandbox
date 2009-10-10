3f5dd5d58ae71febab317021a5adfdc65b92f436
Marek Kubica <marek@xivilization.net>
2009-10-10 22:21:34 +0200

    Added some more function definitions from the little schemer

--- little.scm
+++ little.scm
@@ -114,4 +114,14 @@
       [(eq? (car lat) old) (cons new (multisubst new old (cdr lat)))]
       [else (cons (car lat) (multisubst new old (cdr lat)))])))
 
-(def
+(define o+
+  (lambda (n m)
+    (cond
+      [(zero? m) n]
+      [else (add1 (o+ n (sub1 m)))])))
+
+(define o-
+  (lambda (n m)
+    (cond
+      [(zero? m) n]
+      [else (sub1 (o- n (sub1 m)))])))
