4d831228bcfa7aad8283dff1ac10a26c54b18782
Marek Kubica <marek@xivilization.net>
2009-10-11 15:47:53 +0200

    Added definition of tup+

--- little.scm
+++ little.scm
@@ -137,3 +137,12 @@
     (cond
       [(zero? m) 0]
       [else (o+ n (o* n (sub1 m)))])))
+
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      [(null? tup1) tup2]
+      [(null? tup2) tup1]
+      [else (cons
+             (o+ (car tup1) (car tup2))
+             (tup+ (cdr tup1) (cdr tup2)))])))
