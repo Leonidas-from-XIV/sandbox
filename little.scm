9d68692379438691cfc62ea71a0cecacc14bb465
Marek Kubica <marek@xivilization.net>
2009-10-17 00:07:20 +0200

    Added definition of subst*

--- little.scm
+++ little.scm
@@ -256,3 +256,14 @@
        (add1 (occur* a (cdr l)))]
       [else
        (occur* a (cdr l))])))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+      [(null? l) '()]
+      [(list? (car l))
+       (cons (subst* new old (car l)) (subst* new old (cdr l)))]
+      [(eq? (car l) old)
+       (cons new (subst* new old (cdr l)))]
+      [else
+       (cons (car l) (subst* new old (cdr l)))])))
