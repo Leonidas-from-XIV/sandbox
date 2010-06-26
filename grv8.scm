3bb04178e1be969e74daaf0d0662abfc59f17fdd
Marek Kubica <marek@xivilization.net>
2010-06-26 02:03:08 +0200

    Added more reading, more calculation

--- grv8.scm
+++ grv8.scm
@@ -1,12 +1,29 @@
 (import (rnrs)
         (rnrs io ports)
-	(ypsilon socket))
+        (ypsilon socket))
 
 (define connection-handler
   (lambda (socket)
     (let* ((binary-port (socket-port socket))
            (port (transcoded-port binary-port (make-transcoder (utf-8-codec) 'crlf))))
-      (display (read port)))))
+      (print (get-line port))
+      (say port "HELLO")
+      (print (get-line port))
+      (print (get-line port))
+      (let* ((numbers (map (lambda (dummy)
+                             (string->number (get-line port)))
+                           '(1 2 3)))
+             (sum (fold-left + 0 numbers)))
+        (say port (number->string sum))
+        (print (get-line port))))))
+
+(define say
+  (lambda (port string)
+    (print (string-append "--> " string))
+    (put-string port (string-append string "\r\n"))))
+
+(define (print msg)
+  (display (string-append msg "\n")))
 
 (define main
   (lambda ()
