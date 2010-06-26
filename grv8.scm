4f9d539585099f9415660e79bcd05e13f91e00fc
Marek Kubica <marek@xivilization.net>
2010-06-26 02:08:15 +0200

    Slight cleanups

--- grv8.scm
+++ grv8.scm
@@ -2,32 +2,31 @@
         (rnrs io ports)
         (ypsilon socket))
 
-(define connection-handler
-  (lambda (socket)
+(define (connection-handler socket)
     (let* ((binary-port (socket-port socket))
            (port (transcoded-port binary-port (make-transcoder (utf-8-codec) 'crlf))))
-      (print (get-line port))
+      (hear port)
       (say port "HELLO")
-      (print (get-line port))
-      (print (get-line port))
+      (hear port)
+      (hear port)
       (let* ((numbers (map (lambda (dummy)
                              (string->number (get-line port)))
                            '(1 2 3)))
-             (sum (fold-left + 0 numbers)))
-        (say port (number->string sum))
-        (print (get-line port))))))
+             (sum (fold-left + 0 numbers))
+             (string-sum (number->string sum)))
+        (say port string-sum)
+        (hear port))))
 
-(define say
-  (lambda (port string)
-    (print (string-append "--> " string))
-    (put-string port (string-append string "\r\n"))))
+(define (hear port)
+    (print (get-line port)))
+
+(define (say port string)
+  (print (string-append "--> " string))
+  (put-string port (string-append string "\r\n")))
 
 (define (print msg)
   (display (string-append msg "\n")))
 
-(define main
-  (lambda ()
-    (let ((socket (make-client-socket "ilab.net.in.tum.de" "2342")))
-      (call-with-socket socket connection-handler))))
-
-(main)
+(call-with-socket
+ (make-client-socket "ilab.net.in.tum.de" "2342")
+ connection-handler)
