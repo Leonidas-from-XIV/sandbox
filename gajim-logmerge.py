9dd3c05e22060e4acb723b519fdc38da25bbe954
Marek Kubica <marek@xivilization.net>
2011-05-15 17:30:58 +0200

    Added a bit of structure and a command line parser

--- gajim-logmerge.py
+++ gajim-logmerge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import argparse
 from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
 from sqlalchemy.orm import mapper, sessionmaker, relationship, backref
 from sqlalchemy.orm.exc import NoResultFound
@@ -8,21 +9,6 @@ from sqlalchemy.orm.exc import NoResultFound
 # copied from Gajim common.logger
 JID_NORMAL_TYPE = 0
 JID_ROOM_TYPE = 1
-
-meta_src = MetaData()
-meta_dst = MetaData()
-
-source = create_engine('sqlite:///{}'.format(sys.argv[1]))
-destination = create_engine('sqlite:///{}'.format(sys.argv[2]))
-
-logs_src = Table('logs', meta_src,
-        Column('jid_id', ForeignKey('jids.jid_id')),
-        autoload=True, autoload_with=source)
-jids_src = Table('jids', meta_src, autoload=True, autoload_with=source)
-logs_dst = Table('logs', meta_dst,
-        Column('jid_id', ForeignKey('jids.jid_id')),
-        autoload=True, autoload_with=source)
-jids_dst = Table('jids', meta_dst, autoload=True, autoload_with=source)
 
 class JID(object):
     def __init__(self, jid):
@@ -54,53 +40,80 @@ def print_state(state):
     print(state, end='', file=sys.stderr)
     sys.stderr.flush()
 
-mapper(SourceJID, jids_src)
-mapper(SourceMessage, logs_src, properties={
-    'jid' : relationship(SourceJID)
-})
-mapper(DestinationJID, jids_dst)
-mapper(DestinationMessage, logs_dst, properties={
-    'jid' : relationship(DestinationJID)
-})
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Merges Gajim chat history")
+    parser.add_argument('--source', '-s', action='store', required=True)
+    parser.add_argument('--destination', '-d', action='store', required=True)
+    return parser.parse_args()
 
-SourceSession = sessionmaker(bind=source)
-DestinationSession = sessionmaker(bind=destination)
+def main():
+    args = get_arguments()
 
-session_src = SourceSession()
-session_dst = DestinationSession()
+    meta_src = MetaData()
+    meta_dst = MetaData()
 
-items = session_src.query(SourceMessage)
-print("Processing {} message(s)".format(items.count()))
-for message_src in items.all():
-    # exclude conference messages
-    if message_src.jid.type != JID_NORMAL_TYPE:
-        print_state('U')
-        continue
+    source = create_engine('sqlite:///{}'.format(args.source))
+    destination = create_engine('sqlite:///{}'.format(args.destination))
 
-    try:
-        sender_dst = session_dst.query(DestinationJID).\
-                filter_by(jid=message_src.jid.jid).one()
-    except NoResultFound:
-        # no such sender in DB, create a new one
-        sender_dst = DestinationJID(message_src.jid.jid)
-        session_dst.add(sender_dst)
+    logs_src = Table('logs', meta_src,
+            Column('jid_id', ForeignKey('jids.jid_id')),
+            autoload=True, autoload_with=source)
+    jids_src = Table('jids', meta_src, autoload=True, autoload_with=source)
+    logs_dst = Table('logs', meta_dst,
+            Column('jid_id', ForeignKey('jids.jid_id')),
+            autoload=True, autoload_with=destination)
+    jids_dst = Table('jids', meta_dst, autoload=True, autoload_with=destination)
 
-    message_dst = DestinationMessage(jid=sender_dst, time=message_src.time,
-            message=message_src.message, kind=message_src.kind)
+    mapper(SourceJID, jids_src)
+    mapper(SourceMessage, logs_src, properties={
+        'jid' : relationship(SourceJID)
+    })
+    mapper(DestinationJID, jids_dst)
+    mapper(DestinationMessage, logs_dst, properties={
+        'jid' : relationship(DestinationJID)
+    })
 
-    # check whether such a message already exists in the DB
-    duplicates = session_dst.query(DestinationMessage).\
-            filter_by(message=message_src.message).\
-            filter_by(time=message_src.time)
-    if duplicates.all():
-        # we found a duplicate, skip it
-        print_state('S')
-        continue
+    SourceSession = sessionmaker(bind=source)
+    DestinationSession = sessionmaker(bind=destination)
 
-    # all checks went OK, add the message to the DB
-    session_dst.add(message_dst)
-    print_state('.')
-    session_dst.commit()
+    session_src = SourceSession()
+    session_dst = DestinationSession()
 
-# new-line on exit
-print('', file=sys.stderr)
+    items = session_src.query(SourceMessage)
+    print("Processing {} message(s)".format(items.count()))
+    for message_src in items.all():
+        # exclude conference messages
+        if message_src.jid.type != JID_NORMAL_TYPE:
+            print_state('U')
+            continue
+
+        try:
+            sender_dst = session_dst.query(DestinationJID).\
+                    filter_by(jid=message_src.jid.jid).one()
+        except NoResultFound:
+            # no such sender in DB, create a new one
+            sender_dst = DestinationJID(message_src.jid.jid)
+            session_dst.add(sender_dst)
+
+        message_dst = DestinationMessage(jid=sender_dst, time=message_src.time,
+                message=message_src.message, kind=message_src.kind)
+
+        # check whether such a message already exists in the DB
+        duplicates = session_dst.query(DestinationMessage).\
+                filter_by(message=message_src.message).\
+                filter_by(time=message_src.time)
+        if duplicates.all():
+            # we found a duplicate, skip it
+            print_state('S')
+            continue
+
+        # all checks went OK, add the message to the DB
+        session_dst.add(message_dst)
+        print_state('.')
+        session_dst.commit()
+
+    # new-line on exit
+    print('', file=sys.stderr)
+
+if __name__ == '__main__':
+    main()
