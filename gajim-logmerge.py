f15cbed552670b5a1677c476ce4c6f0705d39a3a
Marek Kubica <marek@xivilization.net>
2011-05-15 13:25:57 +0200

    Added code to copy over messages and create JIDs

--- gajim-logmerge.py
+++ gajim-logmerge.py
@@ -3,6 +3,11 @@
 import sys
 from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
 from sqlalchemy.orm import mapper, sessionmaker, relationship, backref
+from sqlalchemy.orm.exc import NoResultFound
+
+# copied from Gajim common.logger
+JID_NORMAL_TYPE = 0
+JID_ROOM_TYPE = 1
 
 meta_src = MetaData()
 meta_dst = MetaData()
@@ -20,7 +25,9 @@ logs_dst = Table('logs', meta_dst,
 jids_dst = Table('jids', meta_dst, autoload=True, autoload_with=source)
 
 class JID(object):
-    pass
+    def __init__(self, jid):
+        self.jid = jid
+        self.type = JID_NORMAL
 
 class SourceJID(JID):
     pass
@@ -29,12 +36,12 @@ class DestinationJID(JID):
     pass
 
 class Message(object):
-    def __init__(self, jid, time, message):
+    def __init__(self, jid, time, message, kind):
         self.time = time
         self.message = message
         self.jid = jid
         self.contact_name = ''
-        self.kind = 4
+        self.kind = kind
         self.subject = ''
 
 class SourceMessage(Message):
@@ -67,11 +74,31 @@ for message_src in session_src.query(SourceMessage).limit(1):
     print(dir(message_src.jid))
     print(message_src.jid.jid)
 
-    # TODO: create JID if it doesn't exist
-    sender_dst = session_dst.query(DestinationJID).filter_by(jid=message_src.jid.jid).one()
+    # exclude conference messages
+    if message_src.jid.type != JID_NORMAL_TYPE:
+        print('rooms not supported')
+        continue
+
+    try:
+        sender_dst = session_dst.query(DestinationJID).filter_by(jid=message_src.jid.jid+"def").one()
+    except NoResultFound:
+        print("no such jid, creating")
+        sender_dst = DestinationJID(message_src.jid.jid+"def")
+        session_dst.add(sender_dst)
+
     print(sender_dst)
 
-    message_dst = DestinationMessage(jid=sender_dst, time=message_src.time, message=message_src.message)
-    # TODO: check whether message does not exist already in destination DB
+    message_dst = DestinationMessage(jid=sender_dst, time=message_src.time, message=message_src.message, kind=message_src.kind)
     print(message_dst)
+
+    duplicates = session_dst.query(DestinationMessage).\
+            filter_by(message=message_src.message).\
+            filter_by(time=message_src.time)
+    if duplicates.all():
+        print('Message already in DB, skipping')
+        #continue
+
+
     session_dst.add(message_dst)
+    print(session_dst.new)
+    #session_dst.commit()
