0a3f6f408d6fe25e662f2717db36a797e9126abc
Marek Kubica <marek@xivilization.net>
2011-07-27 17:38:37 +0200

    Simpler API, now we take the words explicitly

--- markov.clj
+++ markov.clj
@@ -19,22 +19,23 @@
   (rand-nth (cache words)))
 
 (defn markov-chain
-  ([cache]
+  ([words]
    (let [seed (rand-int (- (count words) 3))
-        seed-first (nth words seed)
-        seed-second (nth words (inc seed))]
+         seed-first (nth words seed)
+         seed-second (nth words (inc seed))
+         ; create the cache automatically
+         cache (reduce transition-cache {} (create-markov-transitions words))]
      (markov-chain cache seed-first seed-second)))
   ([cache word1 word2]
    (let [new-word (markov-link cache [word1 word2])]
      (lazy-seq
        (cons new-word (markov-chain cache word2 new-word))))))
 
-(def cache (reduce transition-cache {} (create-markov-transitions words)))
-
 (with-command-line
   *command-line-args*
   "Usage: blah"
-  [[words w "The number of words to generate" 25]]
-  (let [generated-words (take 25 (markov-chain cache))]
+  [[number n "The number of words to generate" "25"]]
+  (let [number (Integer/parseInt number)
+        generated-words (take number (markov-chain words))]
     (println (str-join " " generated-words))))
 
