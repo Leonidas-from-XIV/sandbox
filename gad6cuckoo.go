c9488ad4b090a2a051f1cbabbcd46e0ec9108954
Marek Kubica <marek@xivilization.net>
2013-06-10 22:52:22 +0200

    Added some error handling

--- gad6cuckoo.go
+++ gad6cuckoo.go
@@ -1,18 +1,19 @@
-// yes, this is indeed incomplete. woe on me!
+// is this working properly? Not entirely sure
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"math"
-	"bytes"
 )
 
 type HashTable struct {
-	one []int
-	two []int
-	h1  func(int) int
-	h2  func(int) int
-	max int
+	one   []int
+	two   []int
+	h1    func(int) int
+	h2    func(int) int
+	max   int
 	first bool
 }
 
@@ -20,15 +21,15 @@ func (h *HashTable) Find(e int) bool {
 	return h.one[h.h1(e)] == e || h.two[h.h2(e)] == e
 }
 
-func (h *HashTable) Insert(e int) {
-	h.insert(e, h.max)
+func (h *HashTable) Insert(e int) error {
+	return h.insert(e, h.max)
 }
 
-func (h *HashTable) insert(e int, retries int) {
+func (h *HashTable) insert(e int, retries int) error {
 	h1 := h.h1(e)
 	h2 := h.h2(e)
 	if h.one[h1] == e || h.two[h2] == e {
-		return
+		return nil
 	}
 
 	h.first = !h.first
@@ -44,15 +45,14 @@ func (h *HashTable) insert(e int, retries int) {
 
 	if relevant[index] == 0 {
 		relevant[index] = e
-		return
+		return nil
 	} else {
-		println("Moving...")
 		if retries == 0 {
-			panic("Maximum retries for insertion reached")
+			return errors.New(fmt.Sprintf("Rejected value %d, does not fit", e))
 		}
 		old := relevant[index]
 		relevant[index] = e
-		h.insert(old, retries - 1)
+		return h.insert(old, retries-1)
 	}
 
 }
@@ -69,13 +69,13 @@ func (h *HashTable) Remove(e int) {
 
 func (h *HashTable) String() string {
 	var buf bytes.Buffer
-	buf.WriteString("Tabelle 1: [ ")
-	for _, v := range(h.one) {
-		buf.WriteString(fmt.Sprintf("%d ", v))
+	buf.WriteString("Table 1: [ ")
+	for _, v := range h.one {
+		buf.WriteString(fmt.Sprintf("%2d ", v))
 	}
-	buf.WriteString("]\nTabelle 2: [ ")
-	for _, v := range(h.two) {
-		buf.WriteString(fmt.Sprintf("%d ", v))
+	buf.WriteString("]\nTable 2: [ ")
+	for _, v := range h.two {
+		buf.WriteString(fmt.Sprintf("%2d ", v))
 	}
 	buf.WriteString("]")
 	return buf.String()
@@ -103,20 +103,59 @@ func get_hash_fn(a []int, k int, p int, n int) func(int) int {
 }
 
 func main() {
-	a1 := []int{4, 5, 8, 9}
+	a1 := []int{2, 4, 16, 8}
 	k1 := len(a1)
-	p1 := 9
-	n1 := 7
-	a2 := []int{4, 5, 8, 9}
-	k2 := len(a2)
-	p2 := 9
-	n2 := 7
+	p1 := 64
+	n1 := 20
+
 	max := 3
 
-	h := NewHashTable(a1, k1, p1, n1, a2, k2, p2, n2, max)
+	h := NewHashTable(a1, k1, p1, n1, a1, k1, p1, n1, max)
 	fmt.Printf("1 exists in h? %t\n", h.Find(1))
 	h.Insert(1)
 	fmt.Printf("%s\n", h)
 	fmt.Printf("1 exists in h? %t\n", h.Find(1))
-	h.Remove(1)
+
+	for i := 0; i < 40; i = i + 3 {
+		fmt.Printf("Inserting %d\n", i)
+		err := h.Insert(i)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+	fmt.Printf("%s\n", h)
+
+	a2 := []int{3, 19, 27, 31}
+	k2 := len(a2)
+	n2 := 53
+	p2 := 29
+	a3 := []int{2, 5, 17, 21}
+	k3 := len(a3)
+	n3 := 33
+	p3 := 35
+
+	fmt.Printf("New HashTable with better parameters\n")
+	h2 := NewHashTable(a2, k2, p2, n2, a3, k3, p3, n3, max)
+	for i := 0; i < 40; i = i + 3 {
+		fmt.Printf("Inserting %d\n", i)
+		err := h2.Insert(i)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+	fmt.Printf("After inserting range(0, 40, 3):\n%s\n", h2)
+	for i := 0; i < 40; i = i + 2 {
+		fmt.Printf("Inserting %d\n", i)
+		err := h2.Insert(i)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+	fmt.Printf("After inserting range(0, 40, 2):\n%s\n", h2)
+
+	for i := 0; i < 44; i = i + 2 {
+		fmt.Printf("Trying to remove %d\n", i)
+		h2.Remove(i)
+	}
+	fmt.Printf("After removing range(0, 44, 2):\n%s\n", h2)
 }
