d8f3b3c7c76cb0ab1a01d36baba1b75ff5937a5d
Marek Kubica <marek@xivilization.net>
2013-06-14 11:56:03 +0200

    Working implementation of MergeSort in Go

--- gad7merge.go
+++ gad7merge.go
@@ -8,8 +8,11 @@ import (
 
 func revmerge(a []int, b []int) []int {
 	if len(a) == 0 {
+		// as this is revmerge, it needs to be reversed
+		reverse(b)
 		return b
 	} else if len(b) == 0 {
+		reverse(a)
 		return a
 	} else {
 		if a[0] <= b[0] {
@@ -20,17 +23,38 @@ func revmerge(a []int, b []int) []int {
 	}
 }
 
-func merge(a []int, b []int) []int {
-	res := revmerge(a, b)
+func reverse(res []int) {
 	// from Effective Go, Y U NO INCLUDE REVERSE?
 	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
 		res[i], res[j] = res[j], res[i]
 	}
+}
+
+func merge(a []int, b []int) []int {
+	res := revmerge(a, b)
+	reverse(res)
 	return res
 }
 
-func mergesort(in []int) []int {
-	return in
+func mergesort(input []int) []int {
+	// create a copy as slices are mutable
+	in := make([]int, len(input))
+	copy(in, input)
+
+	if len(in) <= 1 {
+		return in
+	}
+	if len(in) == 2 {
+		if in[0] < in[1] {
+			return in
+		} else {
+			return []int{in[1], in[0]}
+		}
+	}
+	half := len(in) / 2
+	left := in[:half]
+	right := in[half:]
+	return merge(mergesort(left), mergesort(right))
 }
 
 func main() {
@@ -40,6 +64,6 @@ func main() {
 	for k, _ := range elements {
 		elements[k] = int(r.Float64() * 100)
 	}
-	fmt.Printf("%s\n", elements)
-	fmt.Printf("Merged: %s\n", merge([]int{1, 3}, []int{4, 5}))
+	fmt.Printf("Unsorted: %s\n", elements)
+	fmt.Printf("Sorted:   %s\n", mergesort(elements))
 }
