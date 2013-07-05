244c1cb151a40bb9f6765d5e0b832393563ec844
Marek Kubica <marek@xivilization.net>
2013-07-05 21:08:50 +0200

    Skeleton of binary heap implementation in Go

---
+++ gad10heap.go
@@ -0,0 +1,22 @@
+package main
+
+type BinaryHeap struct {
+	elements []int
+}
+
+func (b *BinaryHeap) Build(entries []int) {
+	b.elements = append(b.elements, entries...)
+	var until = len(entries) / 2
+	for i := 0; i < until; i++ {
+	}
+}
+
+func main() {
+	a := []int{12, 9, 8, 7, 6, 4, 3, 2}
+	b := []int{-24, 16, -302, -17, -56, 8, -1, 22, 54, 92, -70, 33, 61}
+	c := []int{1, 1, 1, 1, 1, 1, 1, 1}
+	test_heap := new(BinaryHeap)
+	test_heap.Build(a)
+	println(b)
+	println(c)
+}
