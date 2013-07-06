4f2aa0aa1527892d16636a7968fbc2a69d82efae
Marek Kubica <marek@xivilization.net>
2013-07-07 01:55:51 +0200

    Implemented bubbling up and down from lecture slides

--- gad10heap.go
+++ gad10heap.go
@@ -8,11 +8,48 @@ type BinaryHeap struct {
 	elements []int
 }
 
+func (b *BinaryHeap) bubble_up(i int) {
+	H := b.elements
+	for i > 0 || H[(i - 1) / 2] > H[i] {
+		H[i], H[(i - 1) / 2] = H[(i - 1) / 2], H[i]
+		i = (i - 1) / 2
+	}
+}
+
+func (b *BinaryHeap) bubble_down(i int) {
+	var m int
+	n := len(b.elements)
+	H := b.elements
+	for 2*i + 1 < n {
+		if (2*i + 2 >= n) {
+			m = 2 * i + 1
+		} else {
+			if H[2*i + 1] < H[2*i + 2] {
+				m = 2*i + 1
+			} else {
+				m = 2*i + 2
+			}
+		}
+		if H[i] <= H[m] {
+			return
+		}
+		H[i], H[m] = H[m], H[i]
+		i = m
+	}
+}
+
 func (b *BinaryHeap) Build(entries []int) {
 	b.elements = append(b.elements, entries...)
 	var until = len(entries) / 2
-	for i := 0; i < until; i++ {
+	for i := until - 1; i >= 0; i-- {
+		//println("Processing", b.elements[i])
+		b.bubble_down(i)
 	}
+}
+
+func (b *BinaryHeap) Insert(v int) {
+	b.elements = append(b.elements, v)
+	b.bubble_up(len(b.elements) - 1)
 }
 
 func (b *BinaryHeap) String() string {
@@ -25,7 +62,7 @@ func main() {
 	c := []int{1, 1, 1, 1, 1, 1, 1, 1}
 	test_heap := new(BinaryHeap)
 	test_heap.Build(a)
-	fmt.Println(test_heap)
+	fmt.Println("test_heap.build(a), should be [2 6 3 7 12 4 8 9], is ", test_heap)
 	fmt.Println(b)
 	fmt.Println(c)
 }
