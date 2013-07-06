82cbb2fdd152771dbd550c0f67a9c7ba66e62352
Marek Kubica <marek@xivilization.net>
2013-07-07 01:25:10 +0200

    Added formatting of results

--- gad10heap.go
+++ gad10heap.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type BinaryHeap struct {
 	elements []int
 }
@@ -11,12 +15,17 @@ func (b *BinaryHeap) Build(entries []int) {
 	}
 }
 
+func (b *BinaryHeap) String() string {
+	return fmt.Sprint(b.elements)
+}
+
 func main() {
 	a := []int{12, 9, 8, 7, 6, 4, 3, 2}
 	b := []int{-24, 16, -302, -17, -56, 8, -1, 22, 54, 92, -70, 33, 61}
 	c := []int{1, 1, 1, 1, 1, 1, 1, 1}
 	test_heap := new(BinaryHeap)
 	test_heap.Build(a)
-	println(b)
-	println(c)
+	fmt.Println(test_heap)
+	fmt.Println(b)
+	fmt.Println(c)
 }
