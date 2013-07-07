995ee82aaeb653e457ca524bb804bbde1a02ccf2
Marek Kubica <marek@xivilization.net>
2013-07-07 02:57:25 +0200

    Style fixes, comments and fixed InsertH return value
    Looks like I'm done with this

--- gad10heap.go
+++ gad10heap.go
@@ -1,3 +1,4 @@
+// Binary Heap implementation
 package main
 
 import (
@@ -8,25 +9,27 @@ type BinaryHeap struct {
 	elements []int
 }
 
+// moves an element up the heap and returns its final index in the heap slice
 func (b *BinaryHeap) bubble_up(i int) int {
 	H := b.elements
-	for i > 0 && H[(i - 1) / 2] > H[i] {
+	for i > 0 && H[(i-1)/2] > H[i] {
 		//println(H[(i-1) / 2], ">", H[i])
-		H[i], H[(i - 1) / 2] = H[(i - 1) / 2], H[i]
+		H[i], H[(i-1)/2] = H[(i-1)/2], H[i]
 		i = (i - 1) / 2
 	}
 	return i
 }
 
+// moves an element down the heap
 func (b *BinaryHeap) bubble_down(i int) {
 	var m int
 	n := len(b.elements)
 	H := b.elements
-	for 2*i + 1 < n {
-		if (2*i + 2 >= n) {
-			m = 2 * i + 1
+	for 2*i+1 < n {
+		if 2*i+2 >= n {
+			m = 2*i + 1
 		} else {
-			if H[2*i + 1] < H[2*i + 2] {
+			if H[2*i+1] < H[2*i+2] {
 				m = 2*i + 1
 			} else {
 				m = 2*i + 2
@@ -40,8 +43,10 @@ func (b *BinaryHeap) bubble_down(i int) {
 	}
 }
 
+// builds a heap from a slice of items
 func (b *BinaryHeap) Build(entries []int) {
 	b.elements = append(b.elements, entries...)
+	// bubble down the first floor(n/2) elements in reverse order
 	var until = len(entries) / 2
 	for i := until - 1; i >= 0; i-- {
 		//println("Processing", b.elements[i])
@@ -49,24 +54,31 @@ func (b *BinaryHeap) Build(entries []int) {
 	}
 }
 
+// inserts an element into the heap
 func (b *BinaryHeap) Insert(v int) {
+	// use InsertH and ignore its return value
 	b.InsertH(v)
 }
 
+// inserts an element into the heap and returns its resulting index
 func (b *BinaryHeap) InsertH(v int) int {
 	b.elements = append(b.elements, v)
-	b.bubble_up(len(b.elements) - 1)
-	return 0
+	return b.bubble_up(len(b.elements) - 1)
 }
 
+// printing a heap should just return the slice printed
 func (b *BinaryHeap) String() string {
 	return fmt.Sprint(b.elements)
 }
 
+// deletes the minimal element
 func (b *BinaryHeap) DeleteMin() int {
+	// … by delegating to removeHV and setting the handle to a 0 since
+	// thats where the minimum is, duh
 	return b.removeHV(0)
 }
 
+// returns the value of the element at index h and deletes it
 func (b *BinaryHeap) removeHV(h int) int {
 	H := b.elements
 	n := len(H)
@@ -82,22 +94,30 @@ func (b *BinaryHeap) removeHV(h int) int {
 	return e
 }
 
+// removes an element at index h
 func (b *BinaryHeap) RemoveH(h int) {
+	// … by ignoring removeHV's return value,
+	// ain't nobody got time for that!
 	b.removeHV(h)
 }
 
+// returns the value of the minimal value
 func (b *BinaryHeap) Min() int {
 	return b.elements[0]
 }
 
+// decreases a the value at index h to value k
 func (b *BinaryHeap) DecreaseKeyH(h int, k int) {
-	// TODO assert that k is smaller than H[h]'s current value
 	H := b.elements
+	if H[h] < k {
+		panic("Key to decrease is lower than new value")
+	}
 	H[h] = k
 	b.bubble_up(h)
 }
 
 func main() {
+	// test data
 	a := []int{12, 9, 8, 7, 6, 4, 3, 2}
 	b := []int{-24, 16, -302, -17, -56, 8, -1, 22, 54, 92, -70, 33, 61}
 	c := []int{1, 1, 1, 1, 1, 1, 1, 1}
