bb05be6a857735106030ab3c633596cfbd8156ab
Marek Kubica <marek@xivilization.net>
2013-08-17 13:31:01 +0200

    Stupid slow link tracker, be gone

--- rennrad-news.user.js
+++ rennrad-news.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name Rennrad-News Textarea-Fix
+// @name Rennrad-News Textarea & Privacy fix
 // @namespace https://github.com/Leonidas-from-XIV/sandbox/
 // @description Fix the cursor navigation on Rennrad News
 // @include http://www.rennrad-news.de/forum/*
@@ -8,7 +8,15 @@
 (function () {
   $(document).ready(function () {
     var area = $('textarea#ctrl_message');
-    // ugly dalay hack to wait for the events to be bound to unbind them
+    // ugly delay hack to wait for the events to be bound to unbind them
     area.delay(500).unbind();
+
+    // also delete tracking link gateway
+    setTimeout(function () {
+      $("a[ao_sl_clk]").removeAttr("ao_sl_clk");
+      $("a[ao_sl_cont]").removeAttr("ao_sl_cont");
+      $("a[ao_sl_blur]").removeAttr("ao_sl_blur");
+      $("a[ao_sl_mouseout]").removeAttr("ao_sl_mouseout");
+    }, 500);
   });
 })();
