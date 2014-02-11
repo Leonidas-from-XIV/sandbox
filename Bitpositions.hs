f0b2f1b6ff8b21f2b91716f71d01320b2a0f4b62
Marek Kubica <marek@xivilization.net>
2014-02-11 10:14:33 +0100

    Solution for bit fiddling

---
+++ Bitpositions.hs
@@ -0,0 +1,32 @@
+module Main where
+import System.Environment (getArgs)
+import Data.Bits ((.&.), shift)
+
+processLine :: String -> String
+processLine l = case map (\x -> (read x::Int)) $ wordsWhen (== ',') l of
+	[n, p1, p2] -> boolToString $ sameBit n (p1-1) (p2-1)
+	otherwise -> "Invalid input"
+
+boolToString :: Bool -> String
+boolToString True = "true"
+boolToString False = "false"
+
+-- get the n-th bit of the number n
+nthBit :: Int -> Int -> Int
+nthBit n p = shift n (-p) .&. 1
+
+-- check if the p1-th bit is the same as the p2-th bit of number n
+sameBit :: Int -> Int -> Int -> Bool
+sameBit n p1 p2 = bitOfN p1 == bitOfN p2
+	where bitOfN = nthBit n
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+	"" -> []
+	s' -> w : wordsWhen p s''
+		where (w, s'') = break p s'
+
+main = do
+	[inpFile] <- getArgs
+	input <- readFile inpFile
+	mapM_ putStrLn $ map processLine $ lines input
