8881d58fa77676aeb0feff22d817b6687cf17015
Marek Kubica <marek@xivilization.net>
2014-02-12 17:10:28 +0100

    Added another solution
    Actually, I'm kinda proud of this funny algorithm I thought of. Not
    super efficient, but GHC does its best to fix it up for me.

---
+++ NumberOfOneBits.hs
@@ -0,0 +1,15 @@
+module Main where
+import System.Environment (getArgs)
+
+-- divides n by 2 iteratively until it reaches 0 and checks how many numbers
+-- are odd, meaning have a 1 as lowest bit
+oneBits :: Int -> Int
+oneBits n = length $ filter odd $ takeWhile (/= 0) $ iterate (flip div 2) n
+
+processLine :: String -> String
+processLine l = show $ oneBits $ (read l::Int)
+
+main = do
+	[inpFile] <- getArgs
+	input <- readFile inpFile
+	mapM_ putStrLn $ map processLine $ lines input
