584cc41f693614a45f54b2452d0a6b68c763e71b
Marek Kubica <marek@xivilization.net>
2014-02-12 17:16:15 +0100

    Style correction by ThreeOfEight, thanks

--- NumberOfOneBits.hs
+++ NumberOfOneBits.hs
@@ -4,7 +4,7 @@ import System.Environment (getArgs)
 -- divides n by 2 iteratively until it reaches 0 and checks how many numbers
 -- are odd, meaning have a 1 as lowest bit
 oneBits :: Int -> Int
-oneBits n = length $ filter odd $ takeWhile (/= 0) $ iterate (flip div 2) n
+oneBits n = length $ filter odd $ takeWhile (/= 0) $ iterate (`div` 2) n
 
 processLine :: String -> String
 processLine l = show $ oneBits $ (read l::Int)
