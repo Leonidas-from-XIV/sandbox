b409ff0498d61b57925626426e2d5d7ba43b0d8e
Marek Kubica <marek@xivilization.net>
2014-02-13 01:49:28 +0100

    Much cleaner solution with much less awful syntax

--- LongestNLines.hs
+++ LongestNLines.hs
@@ -1,14 +1,20 @@
 module Main where
 import System.Environment (getArgs)
 import Data.List (sortBy)
+import Data.Function (on)
 
 lineCollector :: Int -> [String] -> String -> [String]
-lineCollector 0 [] line = []
-lineCollector n [] line = [line]
-lineCollector n (smallest:xs) line = if n > l then (if length line < length smallest then line:smallest:xs else smallest:line:xs)
-	else let (a:b) = sortBy (\a b -> compare (length a) (length b)) (smallest:xs)
-	in (if length line < length a then a else line):b
-	where l = length (smallest:xs)
+lineCollector 0 [] candidate = []
+lineCollector n [] candidate = [candidate]
+lineCollector n (smallest:rest) candidate
+	| n > length rest + 1 =
+		-- the accumulator is not yet filled with n elements
+		if length candidate > length smallest then
+			smallest:candidate:rest
+		else
+			candidate:smallest:rest
+	| length candidate > length smallest = candidate:rest
+	| otherwise = smallest:rest
 
 processLine :: [String] -> [String]
 processLine (n:content) = foldl collector [] content
