c2724c8e20f29df6c065ce8a58d5305515cd5b15
Marek Kubica <marek@xivilization.net>
2014-02-22 14:39:02 +0100

    Solution works for examples now
    Not for whole challenge though

--- Skyscrapers.hs
+++ Skyscrapers.hs
@@ -1,7 +1,7 @@
 module Main where
 import System.Environment (getArgs)
 import Data.Function (on)
-import Data.List (maximumBy, sortBy, groupBy)
+import Data.List (maximumBy, sortBy, groupBy, insertBy, delete)
 --import Debug.Trace (trace)
 
 trace _ x = x
@@ -10,34 +10,40 @@ data Building = Building {l::Int,  h::Int, r::Int} deriving Show
 
 parseTuple :: String -> Building
 parseTuple s = case wordsWhen (== ',') $ takeWhile (/= ')') $ drop 1 s of
-	[l,h,r] -> Building (read l::Int) (read h::Int) (read r::Int)
+	[sl,sh,sr] -> Building (read sl::Int) (read sh::Int) (read sr::Int)
 
 processLine :: String -> String
-processLine l = unwords $ map show $ reformat $ mergeHeights [0] [] bs es
-	where buildings = map parseTuple $ wordsWhen (== ';') l
+processLine line = unwords $ map show $ reformat $ mergeHeights [0] [] bs es
+	where buildings = map parseTuple $ wordsWhen (== ';') line
               bs = begins buildings
               es = ends buildings
 
+begins :: [Building] -> [Building]
 begins xs = map (maximumBy (compare `on` h)) $ groupBy ((==) `on` l) $ sortBy (compare `on` l) xs
+ends :: [Building] -> [Building]
 ends xs = map (maximumBy (compare `on` h)) $ groupBy ((==) `on` r) $ sortBy (compare `on` r) xs
+
+insertOrdered x xs = insertBy (compare `on` negate) x xs
 
 mergeHeights :: [Int] -> [(Int,Int)] -> [Building] -> [Building] -> [(Int,Int)]
 mergeHeights _ [] [] [] = []
+mergeHeights _ res [] [] = res
 -- last building
-mergeHeights _ res [] [last] = (r last, 0):res
+mergeHeights _ res [] [e] = (r e, 0):res
 mergeHeights (c:cs) res [] (e:es)
-	| h e < c = mergeHeights (c:cs) res [] es
+	| h e < c = mergeHeights (c:(delete (h e) cs)) res [] es
 	| h e == c = mergeHeights cs ((r e, head cs):res) [] es
 mergeHeights (c:cs) res (b:bs) (e:es)
-	| l b <= r e && h b >= c = trace "branch 1" $ traceCS (c:cs) $ mergeHeights (h b:c:cs) (((l b), h b):res) bs (e:es)
-	| l b <= r e = trace "branch 2" $ traceCS (c:cs) $ mergeHeights (c:cs) res bs (e:es)
-	| r e < l b && h e < c = trace "branch 3" $ traceCS (c:cs) $ mergeHeights (c:cs) res (b:bs) es
-	| r e < l b && h e == c = trace "branch 4" $ traceCS (c:cs) $ mergeHeights cs ((r e, head cs):res) (b:bs) es
+	| l b <= r e && h b >= c = trace "branch 1" $ traceCS (h b:c:cs) $ mergeHeights (h b:c:cs) (((l b), h b):res) bs (e:es)
+	| l b <= r e = trace "branch 2" $ traceCS (c:(insertOrdered (h b) cs)) $ mergeHeights (c:(insertOrdered (h b) cs)) res bs (e:es)
+	| r e < l b && h e < c = trace "branch 3" $ traceCS (c:(delete (h e) cs)) $ mergeHeights (c:(delete (h e) cs)) res (b:bs) es
+	| r e < l b && h e == c = trace "branch 4" $ traceCS cs $ mergeHeights cs ((r e, head cs):res) (b:bs) es
 
+traceCS :: Show a => a -> t -> t
 traceCS cs = trace $ show cs
 
 reformat :: [(Int,Int)] -> [Int]
-reformat r = concat [[a,b] | (a,b) <- reverse r]
+reformat lat = concat [[a,b] | (a,b) <- reverse lat]
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
@@ -45,6 +51,7 @@ wordsWhen p s =  case dropWhile p s of
 	s' -> w : wordsWhen p s''
 		where (w, s'') = break p s'
 
+main :: IO ()
 main = do
 	[inpFile] <- getArgs
 	input <- readFile inpFile
