b117f6155a2eaad85152ca094b564fc932b5e0ac
Marek Kubica <marek@xivilization.net>
2014-02-25 03:07:17 +0100

    Fewer big lists by using a histogram

--- MajorElement.hs
+++ MajorElement.hs
@@ -1,14 +1,31 @@
 module Main where
 import System.Environment (getArgs)
-import Data.List (sort, group)
 
-majorElement :: [[Int]] -> String
-majorElement [] = "None"
-majorElement [(a:_)] = show a
+increment :: [(Int,Int)] -> Int -> [(Int,Int)]
+increment [] a = [(a, 1)]
+increment ((a,b):xs) a'
+	| a' == a = (a, (b+1)):xs
+	| otherwise = (a,b):(increment xs a')
+
+get :: Int -> [(Int,Int)] -> Int
+get a' ((a,b):xs)
+	| a' == a = b
+	| otherwise = get a' xs
+
+process :: Int -> [Int] -> [(Int,Int)] -> Maybe Int
+process req [] _ = Nothing
+process req (x:xs) map = if frequency > req then Just x else process req xs newmap
+	where frequency = get x newmap
+              newmap = increment map x
+
+showProcess :: Maybe Int -> String
+showProcess Nothing = "None"
+showProcess (Just x) = show x
 
 processLine :: String -> String
-processLine l = majorElement $ filter (\x -> length x > n `quot` 2) $ group $ sort entries
-	where n = length entries
+processLine l = showProcess $ process req entries []
+	where req = n `quot` 2
+	      n = length entries
               entries = read ("[" ++ l ++ "]")::[Int]
 
 main :: IO ()
