fa0cc493918846bc44bc9da4c817d173a0e0d341
Marek Kubica <marek@xivilization.net>
2014-02-25 12:05:14 +0100

    Actually, we don't need to make the items Integers
    That read is horribly, horribly slow.

--- MajorElement.hs
+++ MajorElement.hs
@@ -1,26 +1,26 @@
 module Main where
 import System.Environment (getArgs)
 
-increment :: [(Int,Int)] -> Int -> ([(Int,Int)], Int)
+increment :: [(String,Int)] -> String -> ([(String,Int)], Int)
 increment [] a = ([(a, 1)], 1)
 increment ((a,b):xs) a'
 	| a' == a = ((a, (b+1)):xs, (b+1))
 	| otherwise = ((a,b):map, val)
             where (map, val) = increment xs a'
 
-process :: Int -> [Int] -> [(Int,Int)] -> Maybe Int
+process :: Int -> [String] -> [(String,Int)] -> Maybe String
 process req [] _ = Nothing
 process req (x:xs) map = if frequency > req then Just x else process req xs map'
 	where (map', frequency) = increment map x
 
-showProcess :: Maybe Int -> String
+showProcess :: Maybe String -> String
 showProcess Nothing = "None"
-showProcess (Just x) = show x
+showProcess (Just x) = x
 
 processLine :: String -> String
 processLine l = showProcess $ process req entries []
 	where req = length entries `quot` 2
-	      entries = map (\x -> read x::Int) $ wordsWhen (== ',') l
+	      entries = wordsWhen (== ',') l
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
