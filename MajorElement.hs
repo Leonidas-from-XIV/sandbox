7ecd215a3ffd0bf20c5143a274f18c809d427c39
Marek Kubica <marek@xivilization.net>
2014-02-25 03:19:17 +0100

    Removed double seek in map by returning result immediately

--- MajorElement.hs
+++ MajorElement.hs
@@ -1,22 +1,17 @@
 module Main where
 import System.Environment (getArgs)
 
-increment :: [(Int,Int)] -> Int -> [(Int,Int)]
-increment [] a = [(a, 1)]
+increment :: [(Int,Int)] -> Int -> ([(Int,Int)], Int)
+increment [] a = ([(a, 1)], 1)
 increment ((a,b):xs) a'
-	| a' == a = (a, (b+1)):xs
-	| otherwise = (a,b):(increment xs a')
-
-get :: Int -> [(Int,Int)] -> Int
-get a' ((a,b):xs)
-	| a' == a = b
-	| otherwise = get a' xs
+	| a' == a = ((a, (b+1)):xs, (b+1))
+	| otherwise = ((a,b):map, val)
+            where (map, val) = increment xs a'
 
 process :: Int -> [Int] -> [(Int,Int)] -> Maybe Int
 process req [] _ = Nothing
-process req (x:xs) map = if frequency > req then Just x else process req xs newmap
-	where frequency = get x newmap
-              newmap = increment map x
+process req (x:xs) map = if frequency > req then Just x else process req xs map'
+	where (map', frequency) = increment map x
 
 showProcess :: Maybe Int -> String
 showProcess Nothing = "None"
