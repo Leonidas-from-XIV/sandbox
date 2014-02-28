6bb2c3d6839fe93bec935b8239baffa547b604bd
Marek Kubica <marek@xivilization.net>
2014-02-28 14:58:08 +0100

    Semi-working solution

--- FlaviusJosephus.hs
+++ FlaviusJosephus.hs
@@ -2,10 +2,19 @@ module Main where
 import System.Environment (getArgs)
 
 breakNth _ [] = ([], [])
-breakNth n lat = case splitAt (n-2) lat of
+breakNth n lat = case splitAt (n-1) lat of
 	(skipped,r:rs) -> let (skipped', rest) = breakNth n rs in
-		(skipped ++ skipped', x:r:rest)
-	(skipped, []) -> (skipped, x:[])
+		(skipped ++ skipped', r:rest)
+	(skipped, []) -> (skipped, [])
+
+skipBreak 0 n lat = breakNth n lat
+skipBreak skip n lat = let (_:skipped,matched) = skipBreak (skip-1) n (0:lat)
+	in (skipped,matched)
+
+process _ [] _ = []
+process n lat s = let (skipped, died) = skipBreak s n lat
+                      nextSkip = n - length lat `mod` n - 1 + s in
+	died ++ process n skipped nextSkip
 
 processLine :: String -> String
 processLine l = l
