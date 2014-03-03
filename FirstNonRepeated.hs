541dead46bdafb48f217c24748973d528382f38f
Marek Kubica <marek@xivilization.net>
2014-03-03 12:24:21 +0100

    Shorten one-liner a bit

--- FirstNonRepeated.hs
+++ FirstNonRepeated.hs
@@ -4,7 +4,7 @@ import Data.List (groupBy, minimumBy, sortBy)
 import Data.Function (on)
 
 nonRepeated :: String -> Char
-nonRepeated = snd . minimumBy (compare `on` fst) . map head . filter (\e -> length e == 1) . groupBy (\(_,a) (_,b) -> a == b) . sortBy (compare `on` snd) . zip [0..]
+nonRepeated = snd . minimumBy (compare `on` fst) . map head . filter ((== 1) . length) . groupBy ((==) `on` snd) . sortBy (compare `on` snd) . zip [0..]
 
 processLine :: String -> String
 processLine l = [nonRepeated l]
