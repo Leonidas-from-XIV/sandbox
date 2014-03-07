d9b6925120b7ddb81499183b3ebd44ed13845e5d
Marek Kubica <marek@xivilization.net>
2014-03-07 11:46:17 +0100

    Nicer code for Email Validation

--- EmailValidation.py
+++ EmailValidation.py
@@ -14,14 +14,25 @@ comment = r'(\({ctext}*\)){{0,1}}'.format(ctext=ctext)
 domain = r'[a-zA-Z0-9]([a-zA-Z0-9.-])*'
 quoted_string = "{qcontent}*".format(qcontent=qcontent)
 
-localpart = r'{comment}({dot_atom}|"{quoted_string}"|"{quoted_string}"{dot_atom}|{dot_atom}"{quoted_string}"){comment}'.format(comment=comment, dot_atom=dot_atom, quoted_string=quoted_string)
+localpart = r"""
+    {comment} # might start with a comment
+        (
+        {dot_atom} # might be a normal address
+        |
+        "{quoted_string}" # might be completely quoted
+        |
+        "{quoted_string}"\.{dot_atom} # might start with a quoted string
+        |
+        {dot_atom}\."{quoted_string}" # might end with a quoted string
+        )
+    {comment} # might end with a comment
+    """.format(comment=comment, dot_atom=dot_atom, quoted_string=quoted_string)
 address = r'^{local}@{domain}$'.format(local=localpart, domain=domain)
 
-VALID_EMAIL = re.compile(address)
+VALID_EMAIL = re.compile(address, re.VERBOSE)
 
 def process_line(candidate):
-    match = VALID_EMAIL.match(candidate)
-    return "true" if match else "false"
+    return "true" if VALID_EMAIL.match(candidate) else "false"
 
 def main():
     with open(sys.argv[1]) as f:
