29f74a661e4008f22b3ca5e1f0c81cd2b741443d
Marek Kubica <marek@xivilization.net>
2014-03-08 15:14:19 +0100

    Semi-working solution

--- OverlappingRectangles.hs
+++ OverlappingRectangles.hs
@@ -1,17 +1,31 @@
 module Main where
 import System.Environment (getArgs)
 
---data Rect = Rect {ulx::Int, uly::Int, lrx::Int, lry::Int} 
 data Rect = Rect Int Int Int Int
+data Point = Point Int Int
 
-overlap :: Rect -> Rect -> Bool
-overlap (Rect aulx auly alrx alry) (Rect bulx buly blrx blry)
-	| auly <= buly && auly >= blry && aulx <= bulx && aulx >= blrx = True
-	-- … more cases
+contained :: Rect -> Point -> Bool
+contained (Rect ulx uly lrx lry) (Point x y)
+	| x >= ulx && x <= lrx && y >= lry && y <= uly = True
 	| otherwise = False
 
+edges :: Rect -> [Point]
+edges (Rect ulx uly lrx lry) = [Point ulx uly, Point ulx lry, Point lrx lry, Point lrx uly]
+
+showBool :: Bool -> String
+showBool True = "True"
+showBool _ = "False"
+
 processLine :: String -> String
-processLine l = l
+processLine l = case map (\e -> (read e::Int)) $ wordsWhen (== ',') l of
+	[a,b,c,d,e,f,g,h] -> showBool $ any (contained (Rect e f g h)) $ edges (Rect a b c d)
+	_ -> "Invalid input"
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+	"" -> []
+	s' -> w : wordsWhen p s''
+		where (w, s'') = break p s'
 
 main :: IO ()
 main = do
