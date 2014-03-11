cc30496753ffbf8715ba80a65903dbe8c077f069
Marek Kubica <marek@xivilization.net>
2014-03-11 23:14:55 +0100

    Added "glob"-like parsing

--- StringSearching.hs
+++ StringSearching.hs
@@ -1,12 +1,20 @@
 module Main where
 import System.Environment (getArgs)
 
-matchString :: String -> String -> Bool
-matchString _ [] = True
-matchString [] [_] = False
-matchString (x:xs) [e]
-	| x == e = True
-	| x /= e = matchString xs e
+data Entry = Literal Char | Star deriving (Show)
+
+construct :: String -> [Entry]
+construct "*" = [Star]
+construct [x] = [Literal x]
+construct (x:y:xs) = if x == '\\' && y == '*' then (Literal '*'):(construct xs) else
+	(if x == '*' then Star else Literal x):(construct (y:xs))
+
+--matchString :: String -> String -> Bool
+--matchString _ [] = True
+--matchString [] [_] = False
+--matchString (x:xs) [e]
+--	| x == e = True
+--	| x /= e = matchString xs e
 
 
 processLine :: String -> String
