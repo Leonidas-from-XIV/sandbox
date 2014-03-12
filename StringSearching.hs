ce9afd49f6937b250054b47a19db974f09aa6523
Marek Kubica <marek@xivilization.net>
2014-03-12 21:15:05 +0100

    Fixed up remaining cases, added parsing boilerplate

--- StringSearching.hs
+++ StringSearching.hs
@@ -6,7 +6,8 @@ data Entry = Literal Char | Star deriving (Show)
 type Glob = [Entry]
 
 construct :: String -> Glob
-construct "*" = [Star]
+construct [] = []
+construct ['*'] = [Star]
 construct [x] = [Literal x]
 construct (x:y:xs) = if x == '\\' && y == '*' then (Literal '*'):(construct xs) else
 	(if x == '*' then Star else Literal x):(construct (y:xs))
@@ -15,11 +16,24 @@ matchString :: Glob -> String -> Bool
 matchString [] _ = True
 matchString [Star] _ = True
 matchString [_] [] = False
+matchString (Literal _:_) [] = False
 matchString (Literal g:gs) (x:xs) = x == g && matchString gs xs
-matchString (Star:gs) str = any (== True) $ map (matchString gs) $ tails str
+matchString (Star:gs) str = any id $ map (matchString gs) $ tails str
 
 processLine :: String -> String
-processLine l = l
+processLine l = case wordsWhen (== ',') l of
+	[str,pattern] -> showBool $ matchString (Star:(construct pattern)) str
+	_ -> "Invalid input"
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+	"" -> []
+	s' -> w : wordsWhen p s''
+		where (w, s'') = break p s'
+
+showBool :: Bool -> String
+showBool True = "true"
+showBool _ = "false"
 
 main :: IO ()
 main = do
