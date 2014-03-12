e3b33a2e3c39a1e7ee5e19d1096889eab316dd8b
Marek Kubica <marek@xivilization.net>
2014-03-12 20:54:04 +0100

    String searching works more or less now

--- StringSearching.hs
+++ StringSearching.hs
@@ -1,21 +1,22 @@
 module Main where
 import System.Environment (getArgs)
+import Data.List (tails)
 
 data Entry = Literal Char | Star deriving (Show)
+type Glob = [Entry]
 
-construct :: String -> [Entry]
+construct :: String -> Glob
 construct "*" = [Star]
 construct [x] = [Literal x]
 construct (x:y:xs) = if x == '\\' && y == '*' then (Literal '*'):(construct xs) else
 	(if x == '*' then Star else Literal x):(construct (y:xs))
 
---matchString :: String -> String -> Bool
---matchString _ [] = True
---matchString [] [_] = False
---matchString (x:xs) [e]
---	| x == e = True
---	| x /= e = matchString xs e
-
+matchString :: String -> Glob -> Bool
+matchString _ [] = True
+matchString _ [Star] = True
+matchString [] [_] = False
+matchString (x:xs) (Literal g:gs) = x == g && matchString xs gs
+matchString str (Star:gs) = any (== True) $ map (\e -> matchString e gs) $ tails str
 
 processLine :: String -> String
 processLine l = l
