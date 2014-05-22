aa970615a86507c17aa44b417d0812c08ff25846
Marek Kubica <marek@xivilization.net>
2014-05-22 19:15:45 +0200

    Added GPS preserve and dry-run options

--- strip_some_exif.sh
+++ strip_some_exif.sh
@@ -2,6 +2,17 @@
 
 EXIFTOOL=exiftool
 
+show_help() {
+	cat << EOF
+Usage: ${0##*/} [-hgd] [FILE]
+Strip potentially privacy-relevant EXIF information from files while preserving
+technical information.
+    -h          display this help and exit
+    -g          preserve GPS data
+    -d          dry-run. Just print options and exit
+EOF
+}
+
 type $EXIFTOOL > /dev/null 2>&1 || {
 	echo >&2 "This script requires exiftool but it's not installed."
 	echo -n >&2 "exiftool is available in Debian as libimage-exiftool-perl"
@@ -10,7 +21,12 @@ type $EXIFTOOL > /dev/null 2>&1 || {
 	exit 1;
 }
 
-$EXIFTOOL -all= -tagsFromFile @ \
+GPS_TAGS="-GPSLatitude -GPSLatitudeRef -GPSLongitude -GPSLongitudeRef \
+	-GPSAltitude -GPSAltitudeRef -GPSDateStamp -GPSTimeStamp \
+	-GPSDateTime -GPSTrack -GPSTrackRef -GPSSpeed -GPSSpeedRef \
+	-GPSImgDirection -GPSImgDirectionRef -GPSPitch -GPSRoll"
+
+IMAGE_TAGS="\
 	-Make -Model -LensModel -ApertureValue -FlashSetting -FlashType \
 	-FlashExposureComp -FlashMode -ManufacturersModelName -Orientation \
 	-Software -ModifyDate -Copyright -ExposureTime -FNumber \
@@ -18,5 +34,30 @@ $EXIFTOOL -all= -tagsFromFile @ \
 	-FocusPosition -FocusDistance -ExifImageWidth -ExifImageHeight \
 	-ColorSpace -Rights -ImageWidth -ImageHeight -ImageSize \
 	-ExposureProgram -ExposureMode -Lens -ShutterSpeedValue \
-	-DepthOfField -FieldOfView -FocalLength -XResolution -YResolution \
-	"$@"
+	-DepthOfField -FieldOfView -FocalLength -XResolution -YResolution"
+
+PRESERVE_GPS=false
+DRY_RUN=""
+
+while getopts "ghd" opt; do
+	case "$opt" in
+		h)
+			show_help
+			exit 0
+			;;
+		g)
+			PRESERVE_GPS=true
+			;;
+		d)
+			DRY_RUN="echo"
+			;;
+	esac
+done
+# delete options found
+shift "$((OPTIND-1))"
+
+if [ $PRESERVE_GPS == "false" ]; then
+	GPS_TAGS=""
+fi
+
+$DRY_RUN $EXIFTOOL -all= -tagsFromFile @ $IMAGE_TAGS $GPS_TAGS "$@"
