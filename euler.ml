b26e2b2db0fa3c5ec00018a197dc92f6047d4af2
Marek Kubica <marek@xivilization.net>
2014-10-20 15:00:17 +0200

    Full result output

--- euler.ml
+++ euler.ml
@@ -8,7 +8,7 @@
 
 let prec = 1_000_000
 let max_n = 205_211
-let to_string = Gmp.F.to_string_base_digits ~base:10 ~digits:0
+let mode = Gmp.GMP_RNDN
 
 let euler_fraction n =
   let open Z in
@@ -22,9 +22,9 @@ let euler_fraction n =
 
 let f () =
   let (num, den) = euler_fraction max_n in
-  let znum = Gmp.F.from_string (Z.to_string num) in
-  let zden = Gmp.F.from_string (Z.to_string den) in
-  let euler = Gmp.F.div_prec ~prec znum zden in
-  print_endline @@ to_string euler
+  let znum = Gmp.Z.from_string @@ Z.to_string num in
+  let zden = Gmp.Z.from_string @@ Z.to_string den in
+  let euler = Gmp.FR.from_q_prec ~mode ~prec @@ Gmp.Q.from_zs znum zden in
+  print_endline @@ Gmp.FR.to_string_base_digits ~mode ~base:10 ~digits:0 euler
 
 let () = f ()
