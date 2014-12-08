b2e9a68343bf2f23c08fa1bb27a3c966cc877c9b
Marek Kubica <marek@xivilization.net>
2014-12-08 18:57:47 +0100

    Clean up and generalize code

--- c3.ml
+++ c3.ml
@@ -2,18 +2,7 @@
  * ocamlbuild c3.native
  *)
 
-type hierarchy = Class of (string * hierarchy list)
-
-let o = Class ("O", [])
-let a = Class ("A", [o])
-let b = Class ("B", [o])
-let c = Class ("C", [o])
-let d = Class ("D", [o])
-let e = Class ("E", [o])
-let k1 = Class ("K1", [a; b; c])
-let k2 = Class ("K2", [d; b; e])
-let k3 = Class ("K3", [d; a])
-let z = Class ("Z", [k1; k2; k3])
+type 'a hierarchy = Class of ('a * 'a hierarchy list)
 
 let head = function
   | [] -> []
@@ -25,7 +14,7 @@ let tail = function
 
 let concat_map f l = List.concat @@ List.map f l
 
-let head_not_in_tails (l : hierarchy list list) =
+let head_not_in_tails (l : 'a hierarchy list list) =
   let heads = concat_map head l in
   let tails = concat_map tail l in
   let find_a_head_that_is_not_in_tails acc v = match acc with
@@ -37,23 +26,40 @@ let head_not_in_tails (l : hierarchy list list) =
 let remove to_remove l =
   List.filter (fun e -> e != []) (List.map (List.filter (fun e -> e != to_remove)) l)
 
-let rec merge (l : hierarchy list list) =
+exception No_linearization
+
+let rec merge (l : 'a hierarchy list list) =
   match head_not_in_tails l with
   | Some c -> (match remove c l with
     | [] -> [c]
     | n -> c :: merge n)
-  | None -> failwith "Can't be linearized"
+  | None -> raise No_linearization
 
-let rec c3 = function
+let rec c3_exn = function
   | Class (_, []) as res -> [res]
-  | Class (_, parents) as res -> res :: (merge @@ (List.map c3 parents) @ [parents])
+  | Class (_, parents) as res -> res :: (merge @@ (List.map c3_exn parents) @ [parents])
 
-let show_hierarchy = function
-  | Class (n, _) -> n
-
-let show_hierarchy_list lat =
-  "[" ^ String.concat ", " (List.map show_hierarchy lat) ^ "]"
+let c3 cls = match c3_exn cls with
+  | exception No_linearization -> None
+  | v -> Some v
 
 let () =
+  let rec show_hierarchy = function
+    | Class (n, _) -> n
+  and show_hierarchy_list lat =
+    "[" ^ String.concat ", " (List.map show_hierarchy lat) ^ "]"
+  and o = Class ("O", [])
+  and a = Class ("A", [o])
+  and b = Class ("B", [o])
+  and c = Class ("C", [o])
+  and d = Class ("D", [o])
+  and e = Class ("E", [o])
+  and k1 = Class ("K1", [a; b; c])
+  and k2 = Class ("K2", [d; b; e])
+  and k3 = Class ("K3", [d; a])
+  and z = Class ("Z", [k1; k2; k3])
+  in
   print_endline @@ show_hierarchy z;
-  print_endline @@ show_hierarchy_list @@ c3 z
+  match c3 z with
+  | Some v -> print_endline @@ show_hierarchy_list v
+  | None -> print_endline "No linearization"
