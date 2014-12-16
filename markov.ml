dedbfacfd9dff0aefee10ea677b8a90308e4f70c
Marek Kubica <marek@xivilization.net>
2014-12-17 00:33:10 +0100

    Cleaned up code a bit

--- markov.ml
+++ markov.ml
@@ -4,14 +4,12 @@
 
 open Batteries
 
-module KK = struct
+module KKMap = Map.Make(struct
   type t = string * string
 
   let compare (a1, a2) (b1, b2) =
     if compare a1 b1 == 0 then compare a2 b2 else compare a1 b1
-end
-
-module KKMap = Map.Make(KK)
+end)
 
 let words () =
   let words = IO.read_all IO.stdin in
@@ -23,29 +21,29 @@ let rec partition n step coll =
         taken::partition n step (List.drop step coll)
     | _ -> []
 
-let kv = function
-  | [k1; k2; v] -> ((k1, k2), v)
-  | _ -> failwith "oh well"
-
-let add_to_table tbl (k, v) =
-  let oldval = match KKMap.find k tbl with
-    | xs -> xs
-    | exception Not_found -> [] in
-  KKMap.add k (v::oldval) tbl
-
-let markov_link tbl (k1,k2) =
-  match KKMap.find (k1, k2) tbl with
-    | xs -> let next = Random.choice (List.enum xs) in
-      (next, (k2, next))
-    | exception Not_found -> raise LazyList.No_more_elements
-
 let markov_chain tbl =
+  let markov_link (k1, k2) =
+    match KKMap.find (k1, k2) tbl with
+      | xs -> let next = Random.choice (List.enum xs) in
+        (next, (k2, next))
+      | exception Not_found -> raise LazyList.No_more_elements in
+
   let (start_key, _) = Random.choice (KKMap.enum tbl) in
-  LazyList.from_loop start_key (markov_link tbl)
+  LazyList.from_loop start_key markov_link
 
 let prepare_chain triplets =
-  let parts = List.map kv triplets in
-  List.fold_left add_to_table KKMap.empty parts
+  let triplet_to_tuple = function
+    | [k1; k2; v] -> ((k1, k2), v)
+    | _ -> failwith "Not a triplet" in
+
+  let add_to_table tbl (k, v) =
+    let oldval = match KKMap.find k tbl with
+      | xs -> xs
+      | exception Not_found -> [] in
+    KKMap.add k (v::oldval) tbl in
+
+  List.map triplet_to_tuple triplets
+    |> List.fold_left add_to_table KKMap.empty
 
 let () =
   let number = ref 25 in
