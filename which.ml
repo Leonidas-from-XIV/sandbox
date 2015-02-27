9fcfb9ac92c7a404acf8409dce1a620f02b67455
Marek Kubica <marek@xivilization.net>
2015-02-27 17:50:53 +0100

    Using Core for command line parsing

--- which.ml
+++ which.ml
@@ -12,11 +12,19 @@ let find_paths name paths =
     | _ -> false)
 
 let main () =
-  let exe_name = Sys.argv.(1) in
-  (match Sys.getenv "PATH" with
-    | Some paths -> String.split paths ~on:':'
-    | None -> [])
-  |> find_paths exe_name
-  |> List.iter ~f:print_endline
+  let spec =
+    let open Command.Spec in
+    empty
+    +> anon ("command" %: string) in
+  Command.basic
+    ~summary:"Locate command"
+    spec
+    (fun exe_name () ->
+      (match Sys.getenv "PATH" with
+      | Some paths -> String.split paths ~on:':'
+      | None -> [])
+      |> find_paths exe_name
+      |> List.iter ~f:print_endline)
+  |> Command.run
 
 let () = main ()
