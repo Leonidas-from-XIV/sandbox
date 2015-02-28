b26e4fe1f8b12e57d8573535aa70b8b275a7b3b4
Marek Kubica <marek@xivilization.net>
2015-02-28 01:13:34 +0100

    Using Async for lazy lists

--- which.ml
+++ which.ml
@@ -1,33 +1,38 @@
 (*
- * corebuild which.native
+ * corebuild -pkg async which.native
  *)
 
 open Core.Std
+open Async.Std
 
 let find_paths name paths =
-  List.map paths (fun p -> p ^ "/" ^ name) |>
-  List.filter ~f:(fun path ->
-    match Sys.file_exists path with
-    | `Yes -> true
-    | _ -> false)
+  Pipe.of_list paths
+  |> Pipe.filter_map' ~f:(fun path ->
+    let path = path ^ "/" ^ name in
+    Sys.file_exists path >>= function
+    | `Yes -> return @@ Some path
+    | _ -> return None)
 
 let main () =
   let spec = Command.Spec.(
     empty
-    +> flag "-a" no_arg ~doc:"Foo"
+    +> flag "--all" no_arg
+      ~doc:"Print all matches in PATH, not just the first"
+      ~aliases:["-a"]
     +> anon ("command" %: string)) in
-  Command.basic
-    ~summary:"Locate command"
+  Command.async_basic
+    ~summary:"Write the full path of COMMAND(s) to standard output."
     spec
     (fun all exe_name () ->
       (match Sys.getenv "PATH" with
       | Some paths -> String.split paths ~on:':'
       | None -> [])
       |> find_paths exe_name
-      |> (function
-        | x::_ when not all -> [x]
-        | x -> x)
-      |> List.iter ~f:print_endline)
+      |> (fun p -> if all then return p else
+        (Pipe.read p >>= function
+          | `Ok v -> return @@ Pipe.of_list [v]
+          | _ -> return @@ Pipe.of_list []))
+      >>= Pipe.iter ~f:(fun e -> return @@ print_endline e))
   |> Command.run
 
 let () = main ()
