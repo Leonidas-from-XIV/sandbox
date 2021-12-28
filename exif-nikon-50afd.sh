ca1f53d4ba1c27be4397c18bf3d5a2ccf9db6a50
Marek Kubica <marek@xivilization.net>
2021-12-28 17:36:40 +0100

    Update 50mm Nikkor to name from exiftool overview

--- exif-nikon-50afd.sh
+++ exif-nikon-50afd.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-exec exiftool -Lens="Nikkor 50mm f/1.8D" -ApertureValue="1.8" -FNumber="1.8" -FocalLength="50" $@
-
+exec exiftool -Lens="AF Nikkor 50mm f/1.8D" -ApertureValue="1.8" -FNumber="1.8" -FocalLength="50" $@
