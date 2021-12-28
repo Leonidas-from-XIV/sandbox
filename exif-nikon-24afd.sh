3a16d55a6ad4f7a8a94e76a02ce0adf143e68e69
Marek Kubica <marek@xivilization.net>
2021-12-28 17:36:06 +0100

    Add 24mm and 28mm lens setting scripts

---
+++ exif-nikon-24afd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec exiftool -Lens="AF Nikkor 24mm f/2.8D" -ApertureValue="2.8" -FNumber="2.8" -FocalLength="24" $@
---
+++ exif-nikon-28afd.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec exiftool -Lens="AF Nikkor 28mm f/2.8D" -ApertureValue="2.8" -FNumber="2.8" -FocalLength="28" $@
